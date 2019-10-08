737f2fc30f4d9bba3452bb7ab802a8dd59b300e4
Chris Durbin <christopher.d.durbin@nasa.gov>
2019-10-08 16:08:27 -0400

    HARMONY-64: Return validation errors using JSON and hook the OpenAPI routes into the frontend listener.

--- app/server.ts
+++ app/server.ts
@@ -10,6 +10,7 @@ const { promisify } = require('util');
 const logger = require('./util/log');
 const serviceResponse = require('./backends/service-response');
 const serviceResponseRouter = require('./routers/service-response-router');
+const esi2 = require('./frontends/esi2');
 const router = require('./routers/router');
 
 if (dotenvResult.error) {
@@ -75,6 +76,7 @@ function start(config = {}) {
 
   // Setup the frontend server to handle client requests
   const frontend = buildServer('frontend', appPort, (app) => {
+    esi2.addOpenApiRoutes(app);
     app.use('/', router());
   });
 
