513862fffbb3d673da09006cf71c11ffbaa71ed2
Chris Durbin <christopher.d.durbin@nasa.gov>
2019-10-10 07:26:15 -0400

    HARMONY-64: Added tests and comments.

--- test/eoss/get-granule.js
+++ test/eoss/get-granule.js
@@ -13,15 +13,21 @@ describe('EOSS Endpoint', function () {
 
   describe('EOSS service request', function () {
     it('returns an HTTP 400 "Bad Request" error with explanatory message when the bbox parameter is invalid', async function () {
+      const expectedErrorResponse = {
+        errors: [{
+          path: 'bbox',
+          errorCode: 'minItems.openapi.validation',
+          location: 'query',
+          message: 'should NOT have fewer than 4 items' }] };
       const res = await eossRequest(this.frontend, collection, granule, { bbox: [1, 2] });
       expect(res.status).to.equal(400);
-      expect(res.text).to.equal(JSON.stringify('Query parameter "request" must be one of "GetCapabilities", "GetMap"'));
+      expect(res.body).to.eql(expectedErrorResponse);
     });
 
     it('returns successfully when the bbox parameter is valid', async function () {
       const res = await eossRequest(this.frontend, collection, granule, { bbox: [0, 10, 10, 0] });
       expect(res.status).to.equal(200);
-      expect(res.text).to.equal('Granule service request');
+      expect(res.text).to.equal('Called getGranule');
     });
   });
 });
--- test/helpers/eoss.js
+++ test/helpers/eoss.js
@@ -1,4 +1,3 @@
-// const { before, after } = require('mocha');
 const request = require('supertest');
 
 /**
@@ -36,25 +35,7 @@ function eossLandingPageRequest(app) {
   return request(app).get('/docs/eoss');
 }
 
-// /**
-//  * Adds before/after hooks to run an EOSS request on the given collection and granule
-//  *
-//  * @param {string} collection The CMR Collection ID to perform a service on
-//  * @param {string} granule The CMR Granule ID to perform a service on
-//  * @param {object} query Query parameters to send
-//  * @returns {void}
-//  */
-// function hookEossRequest(collection, granule, query) {
-//   before(async function () {
-//     this.res = await eossRequest(this.frontend, collection, query);
-//   });
-//   after(function () {
-//     delete this.res;
-//   });
-// }
-
 module.exports = {
-//   hookEossRequest,
   eossRequest,
   eossSpecRequest,
   eossLandingPageRequest,
