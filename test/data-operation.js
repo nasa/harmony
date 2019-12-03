71e810226cc6652a6c44f526301d3e52f6ac1c6b
Chris Durbin <christopher.d.durbin@nasa.gov>
2019-12-03 15:04:46 -0500

    HARMONY-47: Add data operation v0.2.0 schema and support multiple versions of the schema for different services.

--- test/data-operation.js
+++ test/data-operation.js
@@ -11,6 +11,7 @@ describe('DataOperation', () => {
         sources: [],
         format: {},
         subset: { bbox: [-130, -45, 130, 45, 100] }, // bounding box has one too many numbers
+        version: '0.2.0',
       });
 
       describe('and its "validate" parameter is not passed', () => {
@@ -22,7 +23,7 @@ describe('DataOperation', () => {
       });
 
       describe('and its "validate" parameter is set to true', () => {
-        const call = () => operation.serialize('0.1.0', true);
+        const call = () => operation.serialize(true);
 
         it('throws an error', () => {
           expect(call).to.throw(TypeError);
@@ -30,14 +31,14 @@ describe('DataOperation', () => {
       });
 
       describe('and its "validate" parameter is set to false', () => {
-        const call = () => operation.serialize('0.1.0', false);
+        const call = () => operation.serialize(false);
 
         it('does not throw an error', () => {
           expect(call).to.not.throw();
         });
 
         it('returns its JSON-serialized model', () => {
-          expect(call()).equal('{"client":"harmony-test","callback":"http://example.com/callback","sources":[],"format":{},"subset":{"bbox":[-130,-45,130,45,100]},"version":"0.1.0"}');
+          expect(call()).equal('{"client":"harmony-test","callback":"http://example.com/callback","sources":[],"format":{},"subset":{"bbox":[-130,-45,130,45,100]},"version":"0.2.0"}');
         });
       });
     });
@@ -49,15 +50,36 @@ describe('DataOperation', () => {
         sources: [],
         format: {},
         subset: { bbox: [-130, -45, 130, 45] }, // Correct bounding box
+        version: '0.2.0',
       });
-      const call = () => operation.serialize('0.1.0', false);
+      const call = () => operation.serialize(false);
 
       it('does not throw an error', () => {
         expect(call).to.not.throw();
       });
 
       it('returns its JSON-serialized model', () => {
-        expect(call()).equal('{"client":"harmony-test","callback":"http://example.com/callback","sources":[],"format":{},"subset":{"bbox":[-130,-45,130,45]},"version":"0.1.0"}');
+        expect(call()).equal('{"client":"harmony-test","callback":"http://example.com/callback","sources":[],"format":{},"subset":{"bbox":[-130,-45,130,45]},"version":"0.2.0"}');
+      });
+    });
+
+    describe('when using the 0.1.0 schema version', () => {
+      const operation = new DataOperation({
+        client: 'harmony-test',
+        callback: 'http://example.com/callback',
+        sources: [],
+        format: {},
+        subset: { bbox: [-130, -45, 130, 45] },
+        version: '0.1.0',
+      });
+      const call = () => operation.serialize(false);
+
+      it('does not throw an error', () => {
+        expect(call).to.not.throw();
+      });
+
+      it('returns its JSON-serialized model without the client parameter and the correct 0.1.0 version', () => {
+        expect(call()).equal('{"callback":"http://example.com/callback","sources":[],"format":{},"subset":{"bbox":[-130,-45,130,45]},"version":"0.1.0"}');
       });
     });
   });
