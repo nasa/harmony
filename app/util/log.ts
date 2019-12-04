82a72cbcd8554c52994777fbe68352f3e70a2698
Chris Durbin <christopher.d.durbin@nasa.gov>
2019-12-04 08:05:27 -0500

    HARMONY-115: Default to JSON logging of messages, but allow text string logging using by setting the TEXT_LOGGER environment variable to 'true'

--- app/util/log.ts
+++ app/util/log.ts
@@ -1,13 +1,61 @@
 const winston = require('winston');
 
-const logger = winston.createLogger({
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.json(),
-  ),
-  transports: [
-    new winston.transports.Console(),
-  ],
-});
+/**
+ * Creates a logger that logs messages in JSON format.
+ *
+ * @returns {Logger} The JSON Winston logger
+ */
+function createJsonLogger() {
+  const jsonLogger = winston.createLogger({
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.json(),
+    ),
+    transports: [
+      new winston.transports.Console(),
+    ],
+  });
+
+  return jsonLogger;
+}
+
+/**
+ * Helper method that formats a string as a log tag only if it is provided
+ *
+ * @param {string} tag The tag string to add
+ * @returns {string} The input string in tag format, or the empty string if tag does not exist
+ */
+function optionalTag(tag) {
+  return tag ? ` [${tag}]` : '';
+}
+
+const textformat = winston.format.printf(
+  (info) => `${info.timestamp} [${info.level}]${optionalTag(info.application)}${optionalTag(info.requestId)}${optionalTag(info.component)}: ${info.message}`,
+);
+
+/**
+ * Creates a logger that log messages as a text string. Useful when testing locally and viewing
+ * logs via a terminal.
+ *
+ * @returns {Logger} The text string Winston logger
+ */
+function createTextLogger() {
+  const textLogger = winston.createLogger({
+    defaultMeta: {},
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.prettyPrint(),
+      winston.format.colorize({ colors: { error: 'red', info: 'blue' } }),
+      textformat,
+    ),
+    transports: [
+      new winston.transports.Console(),
+    ],
+  });
+
+  return textLogger;
+}
+
+const logger = process.env.TEXT_LOGGER === 'true' ? createTextLogger() : createJsonLogger();
 
 module.exports = logger;
