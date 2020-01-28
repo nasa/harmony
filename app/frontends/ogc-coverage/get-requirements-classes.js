97511280cd37b214382e86358b8822ab33b1bbc1
Patrick Quinn <patrick@element84.com>
2020-01-28 16:53:49 -0500

    HARMONY-171: Add routes and schema for OGC API Coverages support

---
+++ app/frontends/ogc-coverage/get-requirements-classes.js
@@ -0,0 +1,9 @@
+module.exports = function getLandingPage(req, res) {
+  res.json({
+    conformsTo: [
+      'http://www.opengis.net/spec/ogcapi-common-1/1.0/conf/core',
+      'http://www.opengis.net/spec/ogcapi-common-1/1.0/conf/collections',
+      'http://www.opengis.net/spec/ogcapi-coverages-1/1.0/conf/core',
+    ],
+  });
+};
