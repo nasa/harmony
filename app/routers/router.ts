6032b7ebcd2a5963eb16bdfdbb78c29ca9b2083a
Chris Durbin <christopher.d.durbin@nasa.gov>
2020-02-21 09:51:18 -0500

    HARMONY-192: If no service is found for a collection perform a no-op service which just returns the download links for all of the granules.

--- app/routers/router.ts
+++ app/routers/router.ts
@@ -69,11 +69,6 @@ function service(fn) {
       if (!req.collections || req.collections.length === 0) {
         throw new NotFoundError('Services can only be invoked when a valid collection is supplied in the URL path before the service name');
       }
-      // Attempts to grab an available backend for the requested operation.
-      // If no such backend exists, this will throw, causing desirable 404s.
-      if (!req.collections.every(services.isCollectionSupported)) {
-        throw new NotFoundError('The requested service is not valid for the given collection');
-      }
       child.info('Running service');
       await fn(req, res, next);
     } catch (e) {
