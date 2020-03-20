065dfe6e2d9cac18f29dedcf9131aa549424755c
James Norton <james@element84.com>
2020-03-20 18:59:52 -0400

    HARMONY-143: Clean up docs for `hookCmr` function

--- test/helpers/stub-cmr.js
+++ test/helpers/stub-cmr.js
@@ -3,11 +3,18 @@ const sinon = require('sinon');
 const cmr = require('../../app/util/cmr');
 
 /**
- * Adds before / after hooks in mocha to replace a function in the the
- * `cmr` module with a function that generates a response with the given
- * status and message
- * @param {string} functionName
- * @param {object} response
+ * Adds before / after hooks in mocha to replace a function in the
+ * `cmr` module with a function that generates the given response
+ *
+ * Example: (`cmrPostSearchBase` returns a 400 status with error message)
+ * ```
+* hookCmr('cmrPostSearchBase',
+  { status: 400,
+    data: { errors: ['Corrupt zip file'] }
+  });
+ * ```
+ * @param {string} functionName The name of the function to stub
+ * @param {object} response The desired response
  * @returns {void}
  */
 function hookCmr(functionName, response) {
