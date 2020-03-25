221a3fd4a1b8d51f68efbd98093ced3326ce00de
James Norton <james@element84.com>
2020-03-25 17:11:02 -0400

    HARMONY-143: Add OpenAPI docs for shapefile POSTs

--- app/frontends/ogc-coverages/index.js
+++ app/frontends/ogc-coverages/index.js
@@ -63,6 +63,7 @@ function addOpenApiRoutes(app) {
       getCoverageRangeType: TODO,
       getCoverageMetadata: TODO,
       getCoverageRangeset,
+      postCoverageRangeset: getCoverageRangeset,
       getCoverageAll: TODO,
     },
   });
