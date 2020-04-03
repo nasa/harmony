ac1fd2f5942b5c87a056ea97dfefcd8557cba9e3
James Norton <james@element84.com>
2020-04-03 14:41:20 -0400

    HARMONY-243: Move shapefile upload middlware to separate file

--- app/util/env.ts
+++ app/util/env.ts
@@ -1,17 +1,47 @@
-const harmonyClientId = process.env.CLIENT_ID || 'harmony-unknown';
-const logLevel = process.env.LOG_LEVEL || 'debug';
-const maxSynchronousGranules = process.env.MAX_SYNCHRONOUS_GRANULES || 1;
-const maxAsynchronousGranules = process.env.MAX_ASYNCHRONOUS_GRANULES || 20;
-const isDevelopment = process.env.NODE_ENV === 'development';
-const objectStoreType = process.env.OBJECT_STORE_TYPE || 's3';
-const uploadBucket = process.env.UPLOAD_BUCKET || process.env.STAGING_BUCKET || 'localStagingBucket';
+const { camelCase } = require('change-case');
 
-module.exports = {
-  harmonyClientId,
-  logLevel,
-  maxSynchronousGranules,
-  maxAsynchronousGranules,
-  isDevelopment,
-  objectStoreType,
-  uploadBucket,
-};
+/**
+ * Add a symbol to module.exports with an appropriate value. This has the drawback that these
+ * config variables don't show up in VS Code autocomplete, but the reduction in repeated
+ * boilerplate code is probably worth it.
+ *
+ * @param {string} envName The environment variable corresponding to the config variable in
+ *   CONSTANT_CASE form
+ * @param {*} defaultValue The value to use if the environment variable is not set. Only strings
+ *   and integers are supported
+ * @returns {void}
+ */
+function makeConfigVar(envName, defaultValue) {
+  const envValue = process.env[envName];
+  let value;
+
+  if (!envValue) {
+    value = defaultValue;
+  } else if (typeof defaultValue === 'number') {
+    value = parseInt(envValue, 10);
+  } else {
+    value = envValue;
+  }
+
+  module.exports[camelCase(envName)] = value;
+}
+
+// create exported config variables
+[
+  // ENV_VAR, DEFAULT_VALUE
+  ['LOG_LEVEL', 'debug'],
+  ['MAX_SYNCHRONOUS_GRANULES', 1],
+  ['MAX_ASYNCHRONOUS_GRANULES', 20],
+  ['.OBJECT_STORE_TYPE', 's3'],
+  // shapefile upload related configs
+  ['MAX_POST_FIELDS', 100],
+  ['MAX_POST_FILE_SIZE', 2000000000],
+  ['MAX_POST_FILE_PARTS', 100],
+].forEach((value) => makeConfigVar.apply(this, value));
+
+
+// special cases
+
+module.exports.harmonyClientId = process.env.CLIENT_ID || 'harmony-unknown';
+module.exports.isDevelopment = process.env.NODE_ENV === 'development';
+module.exports.uploadBucket = process.env.UPLOAD_BUCKET || process.env.STAGING_BUCKET || 'localStagingBucket';
