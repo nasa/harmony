b9e59df2796971584c46f0d54cfdfd2bb798370c
Chris Durbin <christopher.d.durbin@nasa.gov>
2020-04-10 07:06:14 -0400

    HARMONY-263: Add tests for the cloud-access endpoint.

---
+++ test/cloud-access.js
@@ -0,0 +1,75 @@
+const { expect } = require('chai');
+const { describe, it } = require('mocha');
+const { hookServersStartStop } = require('./helpers/servers');
+const { hookCloudAccessJson, hookCloudAccessSh, hookAwsSts, sampleCloudAccessShResponse } = require('./helpers/cloud-access');
+
+describe('Cloud access', function () {
+  hookServersStartStop({ skipEarthdataLogin: false });
+  hookAwsSts();
+
+  describe('When not authenticated', function () {
+    describe('Calls to the cloud access json endpoint', function () {
+      hookCloudAccessJson();
+      it('redirects to Earthdata Login', function () {
+        expect(this.res.statusCode).to.equal(303);
+        expect(this.res.headers.location).to.include(process.env.OAUTH_HOST);
+      });
+    });
+    describe('Calls to the cloud access shell script endpoint', function () {
+      hookCloudAccessSh();
+      it('redirects to Earthdata Login', function () {
+        expect(this.res.statusCode).to.equal(303);
+        expect(this.res.headers.location).to.include(process.env.OAUTH_HOST);
+      });
+    });
+  });
+
+  describe('When authenticated', function () {
+    describe('Calls to the cloud access json endpoint', function () {
+      hookCloudAccessJson('joe-tester');
+
+      it('returns a 200 success', function () {
+        expect(this.res.statusCode).to.equal(200);
+      });
+
+      it('returns a JSON response', function () {
+        expect(this.res.get('Content-Type')).to.equal('application/json; charset=utf-8');
+      });
+
+      it('returns an access key', function () {
+        const { AccessKeyId } = JSON.parse(this.res.text);
+        expect(AccessKeyId).to.match(/^[0-9A-Z]{20}$/);
+      });
+
+      it('returns a secret access key', function () {
+        const { SecretAccessKey } = JSON.parse(this.res.text);
+        expect(SecretAccessKey).to.match(/^\S{40}$/);
+      });
+
+      it('returns a session token', function () {
+        const { SessionToken } = JSON.parse(this.res.text);
+        expect(SessionToken).to.match(/^\S{356}$/);
+      });
+
+      it('returns an expiration datetime', function () {
+        const { Expiration } = JSON.parse(this.res.text);
+        expect(Expiration).to.match(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/);
+      });
+    });
+
+    describe('Calls to the cloud access shell script endpoint', function () {
+      hookCloudAccessSh('joe-tester');
+      it('returns a 200 success', function () {
+        expect(this.res.statusCode).to.equal(200);
+      });
+
+      it('returns a shell script content type', function () {
+        expect(this.res.get('Content-Type')).to.equal('application/x-sh; charset=utf-8');
+      });
+
+      it('returns the correct shell script content', function () {
+        expect(this.res.text).to.equal(sampleCloudAccessShResponse);
+      });
+    });
+  });
+});
---
+++ test/resources/cloud-access-example-response.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Source this file to set keys to access Harmony S3 outputs within the us-west-2 region.
+
+export AccessKeyId='ASIA8NWMTLYQWIFCXH53'
+export SecretAccessKey='Q5DzjpRCxXxgNCbLnsHbec+qDgqQcQZXDd+qEGEc'
+export SessionToken='***REMOVED***'
