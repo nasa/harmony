376f30cfa073919e3a918184ff27e4ef57db787e
Mahabaleshwara Hegde <maha.hegde@nasa.gov>
2020-04-15 15:08:21 -0400

    0. Style changes: tabsize set to 2, switched to single quotes for strings, 1. Removed "use strict" 2. Updated @example section of class documentation 3. Modified throw "..." to throw new TypeError("...") 4. Dropped the fourth argument in the calls to addLink(); was a left over from an earlier version. 5. Changed hasOwnProperty() to Object.hasOwnProperty.call() for checking existence of a property.

--- app/frontends/HarmonyCatalog.js
+++ app/frontends/HarmonyCatalog.js
@@ -1,5 +1,3 @@
-"use strict";
-
 /**
  *
  * Class for creating STAC catalog for data produced by a Harmony Job
@@ -12,55 +10,57 @@
  *
  * @example
  * catalog = new HarmonyCatalog(job);
- * catalog.
+ * jsonObj = catalog.toJSON();
+ * jsonStr = JSON.stringify(catalog, null, 2);
  * @class HarmonyCatalog
  */
 export default class HarmonyCatalog {
-    /**
+  /**
      *
      * @param {Object} job - The Harmony Job object; id, request, and links fields are used.
      * @param {string} job.id - ID of the Harmony Job
      * @param {string} job.requestId - ID of the Harmony Request
      * @param {string} [job.request] - URL for the Harmony Request
+     * @param {Object} [job.links] - Links object in Harmony Job
      */
-    constructor(job) {
-        if (typeof job === "undefined") {
-            throw "Constructor accepts Harmony Job object";
-        }
-        if (!job.hasOwnProperty("id") || !job.hasOwnProperty("requestId")) {
-            throw "Failed to find request and job ID";
-        }
-        // Catalog ID = <requestID>-<jobID>
-        this.id = `${job.requestId}-${job.id}`;
-        this.stac_version = "0.9.0";
-        this.title = `Harmony output (Request= ${job.requestId}, Job=${job.id})`;
-        if (job.hasOwnProperty("request")) {
-            this.description = `Harmony output for ${job.request}`;
-        }
-        this.links = [];
-        this.addLink("./catalog.json", "self", "self", undefined);
-        this.addLink("./catalog.json", "root", "root", undefined);
-        if (job.hasOwnProperty("links") && (Array.isArray(job.links))){
-            for(const index in job.links) {
-                this.addLink("./"+index, "item", job.links[index].title, undefined)
-            }
-        }
+  constructor (job) {
+    if (typeof job === 'undefined') {
+      throw new TypeError('Constructor accepts Harmony Job object')
     }
+    if (!Object.hasOwnProperty.call(job, 'id') || !Object.hasOwnProperty.call(job, 'requestId')) {
+      throw new TypeError('Failed to find request and job ID')
+    }
+    // Catalog ID = <requestID>-<jobID>
+    this.id = `${job.requestId}-${job.id}`
+    this.stac_version = '0.9.0'
+    this.title = `Harmony output (Request= ${job.requestId}, Job=${job.id})`
+    if (Object.hasOwnProperty.call(job, 'request')) {
+      this.description = `Harmony output for ${job.request}`
+    }
+    this.links = []
+    this.addLink('./catalog.json', 'self', 'self', undefined)
+    this.addLink('./catalog.json', 'root', 'root', undefined)
+    if (Object.hasOwnProperty.call(job, 'links') && (Array.isArray(job.links))) {
+      for (const index in job.links) {
+        this.addLink(`./${index}`, 'item', job.links[index].title)
+      }
+    }
+  }
 
-    /**
+  /**
      *
      * @param {string} url - Link URL
      * @param {string} relType - Relation type: [self, root, item]
      * @param {string} title - Link title (human readable)
      */
-    addLink(url, relType, title) {
-        this.links.push({url:url, rel:relType, title:title});
-    }
+  addLink (url, relType, title) {
+    this.links.push({ url: url, rel: relType, title: title })
+  }
 
-    /**
+  /**
      * Placeholder method to support custom stringification
      */
-    toJSON() {
-        return this;
-    }
+  toJSON () {
+    return this
+  }
 }
