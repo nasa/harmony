55e2882959c9a8cce867aa2eb1f2bece2ce88546
Chris Durbin <christopher.d.durbin@nasa.gov>
2020-04-23 09:49:18 -0400

    HARMONY-263: Set the correct environment variables for use with AWS CLI commands.

--- test/resources/cloud-access-example-response.sh
+++ test/resources/cloud-access-example-response.sh
@@ -2,6 +2,6 @@
 # Source this file to set keys to access Harmony S3 outputs within the us-west-2 region.
 # Keys will expire on 2020-04-10T18:03:46.337Z
 
-export AccessKeyId='XXXXXXXXXXXXXXXXXXXX'
-export SecretAccessKey='XXXXXXXXXXXXXXXXXXXX1111111111+++++/////'
-export SessionToken='XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa++++++++++++++++++++++++++++++++++++++++++++++++++00000000000000000000000000000000000000000000000000//////////////////////////////////////////////////XXXXXX=================================================='
+export AWS_ACCESS_KEY_ID='XXXXXXXXXXXXXXXXXXXX'
+export AWS_SECRET_ACCESS_KEY='XXXXXXXXXXXXXXXXXXXX1111111111+++++/////'
+export AWS_SESSION_TOKEN='XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa++++++++++++++++++++++++++++++++++++++++++++++++++00000000000000000000000000000000000000000000000000//////////////////////////////////////////////////XXXXXX=================================================='
