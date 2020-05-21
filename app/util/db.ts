a8d24408bc494e6fa5d26b24a19fdd82832d1dfa
James Norton <james@element84.com>
2020-05-21 15:48:38 -0400

    HARMONY-377: Fixes mocha -w

--- app/util/db.ts
+++ app/util/db.ts
@@ -4,6 +4,7 @@ import knexfile from '../../db/knexfile';
 import knex from 'knex';
 import { attachPaginate } from 'knex-paginate';
 import env from './env';
+import logger from './log';
 
 export type Transaction = knex.Transaction | knex;
 
@@ -11,7 +12,15 @@ const environment = env.nodeEnv;
 const config = knexfile[environment];
 const database = knex(config);
 
-
-attachPaginate();
+// attachPaginate will fail when code is reloaded by mocha -w
+try {
+  attachPaginate();
+} catch (e) {
+  if ((e.message as string).startsWith('Can\'t extend QueryBuilder with existing method (\'paginate\')')) {
+    logger.warn(e.message);
+  } else {
+    throw e;
+  }
+}
 
 export default database;
