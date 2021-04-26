02a211111c0474eb6cd6a0a2298dd9dac7a1a304
Chris Durbin <christopher.d.durbin@nasa.gov>
2021-04-26 12:45:02 -0400

    HARMONY-118: Replace example/dotenv as documentation to a file that is read for default values. Remove default value setting in env.ts instead using the defaults from env-defaults.

--- app/util/env.ts
+++ app/util/env.ts
@@ -1,6 +1,9 @@
+/* eslint-disable guard-for-in */
 import _ from 'lodash';
 import * as dotenv from 'dotenv';
 import * as winston from 'winston';
+import * as fs from 'fs';
+import { isInteger } from './string';
 
 if (Object.prototype.hasOwnProperty.call(process.env, 'GDAL_DATA')) {
   winston.warn('Found a GDAL_DATA environment variable.  This is usually from an external GDAL '
@@ -10,48 +13,8 @@ if (Object.prototype.hasOwnProperty.call(process.env, 'GDAL_DATA')) {
   delete process.env.GDAL_DATA;
 }
 
-if (dotenv.config().error) {
-  winston.warn('Did not read a .env file');
-}
-
-interface HarmonyEnv {
-  logLevel: string;
-  stagingBucket: string;
-  artifactBucket: string;
-  maxSynchronousGranules: number;
-  maxGranuleLimit: number;
-  objectStoreType: string;
-  awsDefaultRegion: string;
-  sameRegionAccessRole: string;
-  maxPostFields: number;
-  maxPostFileSize: number;
-  maxPostFileParts: number;
-  nodeEnv: string;
-  adminGroupId: string;
-  harmonyClientId: string;
-  isDevelopment: boolean;
-  uploadBucket: string;
-  argoUrl: string;
-  cmrEndpoint: string;
-  oauthHost: string;
-  oauthUid: string;
-  useLocalstack: boolean;
-  localstackHost: string;
-  callbackUrlRoot: string;
-  syncRequestPollIntervalMs: number;
-  defaultImagePullPolicy: string;
-  cmrGranuleLocatorImagePullPolicy: string;
-  sharedSecretKey: string;
-  defaultBatchSize: number;
-  defaultParallelism: number;
-  jobReaperPeriodSec: number;
-  reapableJobAgeMinutes: number;
-  defaultArgoPodTimeoutSecs: number;
-  builtInTaskPrefix: string;
-  builtInTaskVersion: string;
-  cmrMaxPageSize: number;
-}
-
+const envDefaults = dotenv.parse(fs.readFileSync('env-defaults'));
+const envOverrides = dotenv.parse(fs.readFileSync('.env'));
 const envVars: HarmonyEnv = {} as HarmonyEnv;
 
 /**
@@ -65,58 +28,62 @@ const envVars: HarmonyEnv = {} as HarmonyEnv;
  * @param defaultValue - The value to use if the environment variable is not set. Only strings
  *   and integers are supported
  */
-function makeConfigVar(envName: string, defaultValue?: string | number): void {
-  const envValue = process.env[envName];
-  let value;
-
-  if (!envValue) {
-    value = defaultValue;
-  } else if (typeof defaultValue === 'number') {
-    value = parseInt(envValue, 10);
+function makeConfigVar(envName: string, defaultValue?: string): void {
+  const stringValue = process.env[envName] || defaultValue;
+  if (isInteger(stringValue)) {
+    envVars[_.camelCase(envName)] = parseInt(stringValue, 10);
   } else {
-    value = envValue;
+    envVars[_.camelCase(envName)] = stringValue;
   }
-
-  envVars[_.camelCase(envName)] = value;
 }
 
-// create exported config variables
-[
-  // ENV_VAR, DEFAULT_VALUE
-  ['ARGO_URL', 'http://localhost:4276'],
-  ['LOG_LEVEL', 'debug'],
-  ['STAGING_BUCKET', 'local-staging-bucket'],
-  ['ARTIFACT_BUCKET', 'local-artifact-bucket'],
-  ['MAX_SYNCHRONOUS_GRANULES', 1],
-  ['MAX_GRANULE_LIMIT', 350],
-  ['OBJECT_STORE_TYPE', 's3'],
-  ['AWS_DEFAULT_REGION', 'us-west-2'],
-  ['SAME_REGION_ACCESS_ROLE'],
-  ['JOB_REAPER_PERIOD_SEC', 360],
-  ['REAPABLE_JOB_AGE_MINUTES', 60],
-  ['LOCALSTACK_HOST', 'localhost'],
-  // shapefile upload related configs
-  ['MAX_POST_FIELDS', 100],
-  ['MAX_POST_FILE_SIZE', 2000000000],
-  ['MAX_POST_FILE_PARTS', 100],
-  ['NODE_ENV', 'development'],
-  ['ADMIN_GROUP_ID', null],
-  ['CMR_ENDPOINT', 'https://cmr.uat.earthdata.nasa.gov'],
-  ['OAUTH_HOST', 'https://uat.urs.earthdata.nasa.gov'],
-  ['OAUTH_UID', null],
-  ['CALLBACK_URL_ROOT', null],
-  ['SYNC_REQUEST_POLL_INTERVAL_MS', 100],
-  ['DEFAULT_BATCH_SIZE', 2000],
-  ['DEFAULT_IMAGE_PULL_POLICY', 'Always'],
-  ['CMR_GRANULE_LOCATOR_IMAGE_PULL_POLICY', 'Always'],
-  ['DEFAULT_PARALLELISM', 2],
-  ['SHARED_SECRET_KEY', null],
-  ['DEFAULT_ARGO_POD_TIMEOUT_SECS', 14400],
-  ['BUILT_IN_TASK_PREFIX', ''],
-  ['BUILT_IN_TASK_VERSION', 'latest'],
-  ['CMR_MAX_PAGE_SIZE', 2000],
-  ['FEEDBACK_URL', null],
-].forEach((value) => makeConfigVar.apply(this, value));
+const envFromFiles = { ...envDefaults, ...envOverrides };
+
+for (const k in envFromFiles) {
+  makeConfigVar(k, envFromFiles[k]);
+}
+
+if (dotenv.config().error) {
+  winston.warn('Did not read a .env file');
+}
+
+interface HarmonyEnv {
+  adminGroupId: string;
+  argoUrl: string;
+  artifactBucket: string;
+  awsDefaultRegion: string;
+  builtInTaskPrefix: string;
+  builtInTaskVersion: string;
+  callbackUrlRoot: string;
+  cmrEndpoint: string;
+  cmrGranuleLocatorImagePullPolicy: string;
+  cmrMaxPageSize: number;
+  defaultArgoPodTimeoutSecs: number;
+  defaultBatchSize: number;
+  defaultImagePullPolicy: string;
+  defaultParallelism: number;
+  harmonyClientId: string;
+  isDevelopment: boolean;
+  jobReaperPeriodSec: number;
+  localstackHost: string;
+  logLevel: string;
+  maxGranuleLimit: number;
+  maxPostFields: number;
+  maxPostFileParts: number;
+  maxPostFileSize: number;
+  maxSynchronousGranules: number;
+  nodeEnv: string;
+  oauthHost: string;
+  oauthUid: string;
+  objectStoreType: string;
+  reapableJobAgeMinutes: number;
+  sameRegionAccessRole: string;
+  sharedSecretKey: string;
+  stagingBucket: string;
+  syncRequestPollIntervalMs: number;
+  uploadBucket: string;
+  useLocalstack: boolean;
+}
 
 // special cases
 
