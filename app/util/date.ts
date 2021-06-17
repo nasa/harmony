043fbbd2e85c290c4f73bfd8fde4dd9667d20273
Chris Durbin <christopher.d.durbin@nasa.gov>
2021-06-17 14:03:40 -0400

    HARMONY-851: Remove default export for toISODateTime.

--- app/util/date.ts
+++ app/util/date.ts
@@ -1,9 +1,10 @@
+/* eslint-disable import/prefer-default-export */
 /**
  * Converts a Date object into an ISO String representation (truncates milliseconds)
  *
  * @param date - The date to convert
  * @returns An ISO string representation of the date, with milliseconds truncated
  */
-export default function toISODateTime(date: Date): string {
+export function toISODateTime(date: Date): string {
   return date.toISOString().replace(/\.\d{3}/g, '');
 }
