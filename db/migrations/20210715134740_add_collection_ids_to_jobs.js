10c949aaf7abbd563a99339a4c4b4fa0464465a5
Chris Durbin <christopher.d.durbin@nasa.gov>
2021-07-20 11:34:46 -0400

    HARMONY-880: Update migration to set a default for collectionIds and then drop that default and make the column not nullable.

--- db/migrations/20210715134740_add_collection_ids_to_jobs.js
+++ db/migrations/20210715134740_add_collection_ids_to_jobs.js
@@ -1,17 +1,16 @@
 exports.up = async function(knex) {
-  await knex.schema.alterTable("jobs", async (t) => {
-    t.text("collectionIds").nullable();
-  });
-
-  await knex.raw(`UPDATE "jobs" SET "collectionIds" = "${JSON.stringify([])}"`);
-
-  await knex.schema.alterTable("jobs", async (t) => {
-    t.text("collectionIds").notNullable();
+  return knex.schema.alterTable('jobs', async (t) => {
+    t.text('collectionIds').defaultTo('[]');
+  })
+  .then(() => {
+    return knex.schema.alterTable('jobs', async (t) => {
+      t.text('collectionIds').notNullable().alter();
+    })
   });
 }
 
 exports.down = async function(knex) {
-  await knex.schema.alterTable("jobs", async (t) => {
-    t.dropColumn("collectionIds");
+  await knex.schema.alterTable('jobs', async (t) => {
+    t.dropColumn('collectionIds');
   });
 }
