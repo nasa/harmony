0ab3072a3405fb92ba67b61895202af5cdc497c4
Hailiang Zhang <hailiang.zhang@nasa.gov>
2021-08-20 16:06:45 -0400

    HARMONY-290: use mergeParameters function in frontends/ogc-coverages/post-coverage-rangeset

--- app/frontends/ogc-coverages/post-coverage-rangeset.ts
+++ app/frontends/ogc-coverages/post-coverage-rangeset.ts
@@ -1,5 +1,5 @@
 import { Response, NextFunction } from 'express';
-import { RequestValidationError } from 'util/errors';
+import { mergeParameters } from 'util/parameter-parsing';
 import getCoverageRangeset from './get-coverage-rangeset';
 import HarmonyRequest from '../../models/harmony-request';
 
@@ -13,8 +13,6 @@ import HarmonyRequest from '../../models/harmony-request';
  * @param req - The request sent by the client
  * @param res - The response to send to the client
  * @param next - The next express handler
- * @throws RequestValidationError - Thrown if the request has validation problems and
- *   cannot be performed
  */
 export default function postCoverageRangeset(
   req: HarmonyRequest,
@@ -22,13 +20,7 @@ export default function postCoverageRangeset(
   next: NextFunction,
 ): void {
   // merge form parameters into the query
-  const queryKeys = Object.keys(req.query);
-  const bodyKeys = Object.keys(req.body);
-  const duplicateKeys = queryKeys.filter((x) => bodyKeys.includes(x));
-  if (duplicateKeys.length) {
-    throw new RequestValidationError(`Duplicate keys "${duplicateKeys}" found from request body and query string!`);
-  }
-  req.query = { ...req.query, ...req.body };
+  mergeParameters(req);
 
   getCoverageRangeset(req, res, next);
 }
