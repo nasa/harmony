6dec0a9d6552e2635c7abb150445699734df2f5b
Chris Durbin <christopher.d.durbin@nasa.gov>
2021-11-16 13:48:28 -0500

    HARMONY-1025: Fix issues with dependencies for query-cmr task.

--- tasks/query-cmr/test/helpers/stac.ts
+++ tasks/query-cmr/test/helpers/stac.ts
@@ -1,20 +1,22 @@
 import fs from 'fs';
 import path from 'path';
-import Ajv from 'ajv';
+import Ajv, { ValidateFunction } from 'ajv';
+import addFormats from 'ajv-formats';
 
 /**
  * Builds a schema validator for the given STAC type ('item' or 'catalog')
  * @param type - The type of validator to build ('item' or 'catalog')
  * @returns the appropriate validator for the given type
  */
-export default function buildStacSchemaValidator(type: 'item' | 'catalog'): Ajv.ValidateFunction {
+export default function buildStacSchemaValidator(type: 'item' | 'catalog'): ValidateFunction {
   const schemaNamesToSchemas: { [name: string]: object } = {};
   fs.readdirSync(path.join(__dirname, '../resources/stac-schemas')).forEach((f) => {
     const filename = path.join(__dirname, '../resources/stac-schemas', f);
-    schemaNamesToSchemas[f.split('.')[0]] = JSON.parse(fs.readFileSync(filename, 'UTF-8'));
+    schemaNamesToSchemas[f.split('.')[0]] = JSON.parse(fs.readFileSync(filename, 'utf8'));
   });
   const schemaDefNames = ['basics', 'datetime', 'instrument', 'licensing', 'provider', 'Feature', 'Geometry'];
   const schemaDefs = schemaDefNames.map((f) => schemaNamesToSchemas[f]);
   const ajv = new Ajv({ schemas: schemaDefs });
+  addFormats(ajv);
   return ajv.compile(schemaNamesToSchemas[type]);
 }
