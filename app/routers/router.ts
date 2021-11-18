736695a1857d980504751d4194ce1910d6f1c010
Hailiang Zhang <hailiang.zhang@nasa.gov>
2021-11-18 17:49:17 -0500

    HARMONY-998: /services/metrics now routing to getReadyWorkItemCountForServiceID

--- app/routers/router.ts
+++ app/routers/router.ts
@@ -13,6 +13,7 @@ import admin from '../middleware/admin';
 import wmsFrontend from '../frontends/wms';
 import { getJobsListing, getJobStatus, cancelJob } from '../frontends/jobs';
 import { getJobs, getJob, getWorkItemsTable } from '../frontends/workflow-ui';
+import { getReadyWorkItemCountForServiceID } from '../frontends/service-metrics';
 import { getStacCatalog, getStacItem } from '../frontends/stac';
 import { getServiceResult } from '../frontends/service-results';
 import cmrGranuleLocator from '../middleware/cmr-granule-locator';
@@ -195,7 +196,7 @@ export default function router({ skipEarthdataLogin = 'false' }: RouterConfig): 
   result.get('/jobs/:jobID', getJobStatus);
   result.post('/jobs/:jobID/cancel', cancelJob);
 
-  result.get('/services/metrics', getVersions);
+  result.get('/services/metrics', getReadyWorkItemCountForServiceID);
 
   result.get('/admin/jobs', getJobsListing);
   result.get('/admin/jobs/:jobID', getJobStatus);
