6586283f9d32edc8a0166860ab0fb41e00095100
Hailiang Zhang <hailiang.zhang@nasa.gov>
2021-11-23 14:56:06 -0500

    HARMONY-998: add more tests in test/helpers/service-metrics.ts

--- test/service-metrics.ts
+++ test/service-metrics.ts
@@ -25,7 +25,24 @@ describe('Backend service metrics endpoint', function () {
     it('returns expected message', function () {
       expect(this.res.text).to.equal(`required parameter "serviceID" was not provided`);
     });
-
   });
+
+  describe('when hitting the service/metrics endpoint without non-existing serviceID', function () {
+    const serviceID = "noexisting/service:version";
+    hookServiceMetrics(serviceID);
+
+    it('returns 404 status code', function () {
+      expect(this.res.statusCode).to.equal(404);
+    });
+
+    it('returns text content', function () {
+      expect(this.res.get('Content-Type')).to.equal('text/html; charset=utf-8');
+    });
+
+    it('returns expected message', function () {
+      expect(this.res.text).to.equal(`service [${serviceID}] does not exist`);
+    });
+  });
+
 
 });
