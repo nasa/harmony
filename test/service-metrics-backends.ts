e94a498a1dbee75e20a22a6f1cf640ad2c600f94
Hailiang Zhang <hailiang.zhang@nasa.gov>
2021-11-23 17:37:20 -0500

    HARMONY-998: fix lint issues in the test

--- test/service-metrics-backends.ts
+++ test/service-metrics-backends.ts
@@ -1,11 +1,7 @@
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
-import { stub } from 'sinon';
 import hookServersStartStop from './helpers/servers';
-import { hookServices } from './helpers/stub-service';
 import { hookServiceMetrics } from './helpers/service-metrics';
-import env from '../app/util/env';
-import hookDescribeImage from './helpers/container-registry';
 
 describe('Backend service metrics endpoint', function () {
 
@@ -23,11 +19,11 @@ describe('Backend service metrics endpoint', function () {
     });
 
     it('returns expected message', function () {
-      expect(this.res.text).to.equal(`required parameter "serviceID" was not provided`);
+      expect(this.res.text).to.equal(`required parameter \"serviceID\" was not provided`);
     });
   });
 
-  describe('when hitting the service/metrics endpoint without non-existing serviceID', function () {
+  describe('when hitting the service/metrics endpoint with a non-existing serviceID', function () {
     const serviceID = "noexisting/service:version";
     hookServiceMetrics(serviceID);
 
