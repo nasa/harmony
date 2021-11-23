2710854f03019e3ec017a04209f1cffa1ce06565
Hailiang Zhang <hailiang.zhang@nasa.gov>
2021-11-23 14:49:43 -0500

    HARMONY-998: format test in test/helpers/service-metrics.ts

--- test/service-metrics.ts
+++ test/service-metrics.ts
@@ -7,25 +7,25 @@ import { hookServiceMetrics } from './helpers/service-metrics';
 import env from '../app/util/env';
 import hookDescribeImage from './helpers/container-registry';
 
-describe('service/metrics endpoint', function () {
+describe('Backend service metrics endpoint', function () {
+
   hookServersStartStop({ skipEarthdataLogin: true });
-  describe('when using the services from services.yml', function () {
-    describe('when hitting the service/metrics endpoint without serviceID parameter', function () {
-      hookServiceMetrics();
+  
+  describe('when hitting the service/metrics endpoint without serviceID parameter', function () {
+    hookServiceMetrics();
 
-      it('returns 400 status code', function () {
-        expect(this.res.statusCode).to.equal(400);
-      });
-
-      it('returns text content', function () {
-        expect(this.res.get('Content-Type')).to.equal('text/html; charset=utf-8');
-      });
-
-      it('returns expected message', function () {
-        expect(this.res.text).to.equal(`required parameter "serviceID" was not provided`);
-      });
-
+    it('returns 400 status code', function () {
+      expect(this.res.statusCode).to.equal(400);
     });
+
+    it('returns text content', function () {
+      expect(this.res.get('Content-Type')).to.equal('text/html; charset=utf-8');
+    });
+
+    it('returns expected message', function () {
+      expect(this.res.text).to.equal(`required parameter "serviceID" was not provided`);
+    });
+
   });
 
 });
