d8275560f236eb2f067b46cf0cab3d12e0170047
Hailiang Zhang <hailiang.zhang@nasa.gov>
2021-12-07 22:10:58 -0500

    HARMONY-999: move stuff to _getHarmonyMetric function

--- tasks/service-runner/app/service/service-metrics.ts
+++ tasks/service-runner/app/service/service-metrics.ts
@@ -6,16 +6,11 @@ import { sanitizeImage } from '../../../../app/util/string';
 import logger from '../../../../app/util/log';
 
 /**
- * Express.js handler that generates the Prometheus compatible metrics for the associated service
+ * Call a service to perform some work
  *
- * @param req - The request sent by the client
- * @param res - The response to send to the client
- * @param next - The next function in the call chain
- * @returns Resolves when the request is complete
+ * @param workItem - the work to be done
  */
-export async function generateMetricsForPrometheus(
-  req: Request, res: Response, next: NextFunction,
-): Promise<void> {
+ async function _getHarmonyMetric(): Promise<string> {
 
   const workUrl = `http://${env.backendHost}:${env.backendPort}/service/metrics`;
   const serviceName = sanitizeImage(env.harmonyService);
@@ -44,7 +39,24 @@ export async function generateMetricsForPrometheus(
       });
 
   const harmony_metric = `# HELP ready_work_items_count Ready work items count for a harmony task-runner service.
-# TYPE ready_work_items_count gauge
-ready_work_items_count{service_id="${serviceName}"} ${response.data.availableWorkItems}`;
+  # TYPE ready_work_items_count gauge
+  ready_work_items_count{service_id="${serviceName}"} ${response.data.availableWorkItems}`;
+
+  return harmony_metric;
+}
+
+/**
+ * Express.js handler that generates the Prometheus compatible metrics for the associated service
+ *
+ * @param req - The request sent by the client
+ * @param res - The response to send to the client
+ * @param next - The next function in the call chain
+ * @returns Resolves when the request is complete
+ */
+export async function generateMetricsForPrometheus(
+  req: Request, res: Response, next: NextFunction,
+): Promise<void> {
+
+  const harmony_metric = _getHarmonyMetric(); 
   res.send(harmony_metric);
 }
