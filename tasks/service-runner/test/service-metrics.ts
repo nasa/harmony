a66c065ed671e4dbca12099be9af369e010db55b
Hailiang Zhang <hailiang.zhang@nasa.gov>
2021-12-08 14:42:31 -0500

    HARMONY-999: fix test

--- tasks/service-runner/test/service-metrics.ts
+++ tasks/service-runner/test/service-metrics.ts
@@ -16,21 +16,29 @@ const { _getHarmonyMetric } = exportedForTesting;
 describe('Service Metrics', async function () {
 
   let mock;
+  const serviceID = 'harmonyservices/query-cmr:latest';
   before(function () {
     mock = new MockAdapter(axios);
+    env.harmonyService = serviceID;
   });
 
   it('Get 200 response', async function () {
-    const harmonyService = 'harmonyservices/query-cmr:latest';
-    env.harmonyService = harmonyService;
     const harmony_metric = `# HELP ready_work_items_count Ready work items count for a harmony task-runner service.
 # TYPE ready_work_items_count gauge
-ready_work_items_count{service_id="${harmonyService}"} 0`;
+ready_work_items_count{service_id="${serviceID}"} 0`;
     mock.onGet().reply(200, {availableWorkItems: 0});
-    const res = await _getHarmonyMetric();
+    const res = await _getHarmonyMetric(serviceID);
     expect(res).to.equal(harmony_metric);
   });
 
+/*
+  it('Get error response', async function () {
+    const serviceID = 'harmonyservices/query-cmr:latest';
+    mock.onGet().reply(500);
+    const res = await _getHarmonyMetric();
+    expect(_getHarmonyMetric).to.throw();
+  });
+*/
   afterEach(function () {
     mock.restore();
   });
