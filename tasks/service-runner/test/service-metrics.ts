024dea6bd13e9d3afd206e132b176fa00b8edf35
Hailiang Zhang <hailiang.zhang@nasa.gov>
2021-12-08 15:41:18 -0500

    HARMONY-999: clean test

--- tasks/service-runner/test/service-metrics.ts
+++ tasks/service-runner/test/service-metrics.ts
@@ -10,33 +10,38 @@ import request from 'supertest';
 
 describe('Service Metrics', async function () {
 
-  const serviceID = 'harmonyservices/query-cmr:latest';
-  const { _getHarmonyMetric } = exportedForTesting;
   let mock;
+  const { _getHarmonyMetric } = exportedForTesting;
   before(function () {
     mock = new MockAdapter(axios);
-    env.harmonyService = serviceID;
   });
 
-  it('Get 200 response', async function () {
-    const harmony_metric = `# HELP ready_work_items_count Ready work items count for a harmony task-runner service.
+  describe('when prometheus pulls the metric', async function () {
+    const serviceID = 'harmonyservices/query-cmr:latest';
+    before(function () {
+      env.harmonyService = serviceID;
+    });
+
+    it('runs successfully', async function () {
+      const harmony_metric = `# HELP ready_work_items_count Ready work items count for a harmony task-runner service.
 # TYPE ready_work_items_count gauge
 ready_work_items_count{service_id="${serviceID}"} 0`;
-    mock.onGet().reply(200, {availableWorkItems: 0});
-    const res = await _getHarmonyMetric(serviceID);
-    expect(res).to.equal(harmony_metric);
-  });
+      mock.onGet().reply(200, {availableWorkItems: 0});
+      const res = await _getHarmonyMetric(serviceID);
+      expect(res).to.equal(harmony_metric);
+    });
 
-/*
-  it('Get error response', async function () {
-    const serviceID = 'harmonyservices/query-cmr:latest';
-    mock.onGet().reply(500);
-    const res = await _getHarmonyMetric();
-    expect(_getHarmonyMetric).to.throw();
-  });
-*/
-  afterEach(function () {
-    mock.restore();
+  /*
+    it('Get error response', async function () {
+      const serviceID = 'harmonyservices/query-cmr:latest';
+      mock.onGet().reply(500);
+      const res = await _getHarmonyMetric();
+      expect(_getHarmonyMetric).to.throw();
+    });
+  */
+    afterEach(function () {
+      mock.restore();
+      });
   });
 
 });
