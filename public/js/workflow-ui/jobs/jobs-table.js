9cf2b5e8eafe516fe026a71934d86084daba75c6
vinny <vinny.inverso@gmail.com>
2022-01-05 11:45:11 -0500

    HARMONY-948: Get rid of mode now that none needs it

--- public/js/workflow-ui/jobs/jobs-table.js
+++ public/js/workflow-ui/jobs/jobs-table.js
@@ -1,34 +1,11 @@
 import { formatDates, initTooltips } from "../table.js";
 
-/**
- * Display indicators of whether a turbo flag was addded to a request.
- */
-function decorateTurboRequests() {
-  const requestCodes = document.getElementsByClassName('mode-td');
-  [...requestCodes].forEach(
-    (element) => {
-      const request = element.getAttribute('data-job-url');
-      if (request.indexOf('turbo=true') > -1) {
-        element.textContent = '🚀';
-        element.setAttribute('title', 'turbo=true was requested');
-      } else if (request.indexOf('turbo=false') > -1) {
-        element.textContent = '🐙';
-        element.setAttribute('title', 'turbo=false was requested');
-      } else {
-        element.textContent = '🚫';
-        element.setAttribute('title', 'turbo=[true|false] was not specified');
-      }
-    }
-  );
-}
-
 export default {
 
   /**
    * Handles jobs table logic.
    */
   async init() {
-    decorateTurboRequests();
     formatDates();
     initTooltips('[data-bs-toggle="tooltip"]');
   }
