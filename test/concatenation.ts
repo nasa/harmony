e022cf4d9402c38790c2729289b5654331e2c370
Chris Durbin <christopher.d.durbin@nasa.gov>
2022-02-11 12:08:38 -0500

    HARMONY-1068: Updates so that harmony tells netcdf-to-zarr services by default to attempt to concatenate the results.

--- test/concatenation.ts
+++ test/concatenation.ts
@@ -6,128 +6,172 @@ import { hookRangesetRequest } from './helpers/ogc-api-coverages';
 import hookServersStartStop from './helpers/servers';
 import StubService from './helpers/stub-service';
 
+describe('testing concatenation', function () {
+  describe('for a CONCISE workflow', function () {
+    const collection = 'C1234208438-POCLOUD';
+    const serviceTag = 'ghcr.io/podaac/concise:sit';
 
-describe('CONCISE workflow', function () {
-  const collection = 'C1234208438-POCLOUD';
-  const serviceTag = 'ghcr.io/podaac/concise:sit';
+    describe('When passing the concatenate parameter', function () {
+      hookServersStartStop( { skipEarthdataLogin: false });
 
-  describe('When passing the concatenate parameter', function () {
-    hookServersStartStop({ skipEarthdataLogin: false });
+      describe('calling the backend service', function () {
+        const query = {
+          concatenate: true,
+          maxResults: 2,
+        };
+        StubService.hook({ params: { redirect: 'http://example.com' } });
+        hookRangesetRequest('1.0.0', collection, 'all', { query, username: 'joe' });
+        it('sets the concatenate flag on the operation', function () {
+          expect(this.service.operation.shouldConcatenate).to.equal(true);
+        });
+      });
 
-    describe('calling the backend service', function () {
+      describe('and it is not set', function () {
+        const query = {
+          maxResults: 2,
+        };
+        hookRangesetRequest('1.0.0', collection, 'all', { query, username: 'joe' });
+        hookRedirect('joe');
+
+        it('does not invoke the service', async function () {
+          const job = JSON.parse(this.res.text);
+          const workflowSteps = await getWorkflowStepsByJobId(db, job.jobID);
+
+          expect(workflowSteps.find((value): boolean => value.serviceID === serviceTag)).to.be.undefined;
+        });
+
+        it('has the `hasAggregatedOutput` flag set to false on the workflow step', async function () {
+          const job = JSON.parse(this.res.text);
+          const workflowSteps = await getWorkflowStepsByJobId(db, job.jobID);
+
+          expect(workflowSteps[1].hasAggregatedOutput).to.equal(0);
+        });
+      });
+
+      describe('and it is set to false', function () {
+        const query = {
+          concatenate: false,
+          maxResults: 2,
+        };
+        hookRangesetRequest('1.0.0', collection, 'all', { query, username: 'joe' });
+        hookRedirect('joe');
+
+        it('does not invoke the service', async function () {
+          const job = JSON.parse(this.res.text);
+          const workflowSteps = await getWorkflowStepsByJobId(db, job.jobID);
+
+          expect(workflowSteps.find((value): boolean => value.serviceID === serviceTag)).to.be.undefined;
+        });
+
+        it('has the `hasAggregatedOutput` flag set to false on the workflow step', async function () {
+          const job = JSON.parse(this.res.text);
+          const workflowSteps = await getWorkflowStepsByJobId(db, job.jobID);
+
+          expect(workflowSteps[1].hasAggregatedOutput).to.equal(0);
+        });
+      });
+
+      describe('and it is set to true', function () {
+        const query = {
+          concatenate: true,
+          maxResults: 2,
+        };
+        hookRangesetRequest('1.0.0', collection, 'all', { query, username: 'joe' });
+        hookRedirect('joe');
+
+        it('invokes the service', async function () {
+          const job = JSON.parse(this.res.text);
+          const workflowSteps = await getWorkflowStepsByJobId(db, job.jobID);
+
+          expect(workflowSteps[1].serviceID).to.eql(serviceTag);
+        });
+
+        it('has the `hasAggregatedOutput` flag set to true on the workflow step', async function () {
+          const job = JSON.parse(this.res.text);
+          const workflowSteps = await getWorkflowStepsByJobId(db, job.jobID);
+
+          expect(workflowSteps[1].hasAggregatedOutput).to.equal(1);
+        });
+      });
+
+      describe('and it is set to something besides true or false', function () {
+        const badQuery = {
+          concatenate: 'random',
+          maxResults: 2,
+        };
+
+        hookRangesetRequest('1.0.0', collection, 'all', { query: badQuery, username: 'joe' });
+
+        it('returns an error', async function () {
+
+          expect(this.res.statusCode).to.equal(400);
+          expect(this.res.body).to.eql({
+            code: 'harmony.RequestValidationError',
+            description: 'Error: query parameter "concatenate" must be \'false\' or \'true\'',
+          });
+        });
+      });
+
+      describe('is requested for a collection that has no service that provides concatenation', function () {
+        const nonConcatCollection = 'C1104-PVC_TS2';
+        const query = {
+          concatenate: 'true',
+          maxResults: 2,
+        };
+
+        hookRangesetRequest('1.0.0', nonConcatCollection, 'all', { query, username: 'joe' });
+
+        it('returns a Not Found Error', async function () {
+
+          expect(this.res.statusCode).to.eql(404);
+          expect(this.res.body).to.eql({
+            code: 'harmony.NotFoundError',
+            description: 'Error: no matching service',
+          });
+        });
+      });
+    });
+  });
+
+  describe('for a netcdf-to-zarr workflow', function () {
+    const zarrCollection = 'C1233800302-EEDTEST';
+    hookServersStartStop();
+
+    describe('when making a request that calls the netcdf-to-zarr backend service', function () {
       const query = {
-        concatenate: true,
         maxResults: 2,
+        format: 'application/x-zarr',
       };
       StubService.hook({ params: { redirect: 'http://example.com' } });
-      hookRangesetRequest('1.0.0', collection, 'all', { query, username: 'joe' });
-      it('sets the concatenate flag on the operation', function () {
+      hookRangesetRequest('1.0.0', zarrCollection, 'all', { query });
+      it('sets the concatenate flag on the operation to be true by default', function () {
         expect(this.service.operation.shouldConcatenate).to.equal(true);
       });
     });
 
-    describe('and it is not set', function () {
+    describe('when explicitly passing the concatenate flag=false', function () {
       const query = {
         maxResults: 2,
-      };
-      hookRangesetRequest('1.0.0', collection, 'all', { query, username: 'joe' });
-      hookRedirect('joe');
-
-      it('does not invoke the service', async function () {
-        const job = JSON.parse(this.res.text);
-        const workflowSteps = await getWorkflowStepsByJobId(db, job.jobID);
-
-        expect(workflowSteps.find((value): boolean => value.serviceID === serviceTag)).to.be.undefined;
-      });
-
-      it('has the `hasAggregatedOutput` flag set to false on the workflow step', async function () {
-        const job = JSON.parse(this.res.text);
-        const workflowSteps = await getWorkflowStepsByJobId(db, job.jobID);
-
-        expect(workflowSteps[1].hasAggregatedOutput).to.equal(0);
-      });
-    });
-
-    describe('and it is set to false', function () {
-      const query = {
+        format: 'application/x-zarr',
         concatenate: false,
-        maxResults: 2,
       };
-      hookRangesetRequest('1.0.0', collection, 'all', { query, username: 'joe' });
-      hookRedirect('joe');
-
-      it('does not invoke the service', async function () {
-        const job = JSON.parse(this.res.text);
-        const workflowSteps = await getWorkflowStepsByJobId(db, job.jobID);
-
-        expect(workflowSteps.find((value): boolean => value.serviceID === serviceTag)).to.be.undefined;
-      });
-
-      it('has the `hasAggregatedOutput` flag set to false on the workflow step', async function () {
-        const job = JSON.parse(this.res.text);
-        const workflowSteps = await getWorkflowStepsByJobId(db, job.jobID);
-
-        expect(workflowSteps[1].hasAggregatedOutput).to.equal(0);
+      StubService.hook({ params: { redirect: 'http://example.com' } });
+      hookRangesetRequest('1.0.0', zarrCollection, 'all', { query });
+      it('sets the concatenate flag on the operation to be false', function () {
+        expect(this.service.operation.shouldConcatenate).to.equal(false);
       });
     });
 
-    describe('and it is set to true', function () {
+    describe('when explicitly passing the concatenate flag=true', function () {
       const query = {
+        maxResults: 2,
+        format: 'application/x-zarr',
         concatenate: true,
-        maxResults: 2,
       };
-      hookRangesetRequest('1.0.0', collection, 'all', { query, username: 'joe' });
-      hookRedirect('joe');
-
-      it('invokes the service', async function () {
-        const job = JSON.parse(this.res.text);
-        const workflowSteps = await getWorkflowStepsByJobId(db, job.jobID);
-
-        expect(workflowSteps[1].serviceID).to.eql(serviceTag);
-      });
-
-      it('has the `hasAggregatedOutput` flag set to true on the workflow step', async function () {
-        const job = JSON.parse(this.res.text);
-        const workflowSteps = await getWorkflowStepsByJobId(db, job.jobID);
-
-        expect(workflowSteps[1].hasAggregatedOutput).to.equal(1);
-      });
-    });
-
-    describe('and it is set to something besides true or false', function () {
-      const badQuery = {
-        concatenate: 'random',
-        maxResults: 2,
-      };
-
-      hookRangesetRequest('1.0.0', collection, 'all', { query: badQuery, username: 'joe' });
-
-      it('returns an error', async function () {
-
-        expect(this.res.statusCode).to.equal(400);
-        expect(this.res.body).to.eql({
-          code: 'harmony.RequestValidationError',
-          description: 'Error: query parameter "concatenate" must be \'false\' or \'true\'',
-        });
-      });
-    });
-
-    describe('is requested for a collection that has no service that provides concatenation', function () {
-      const nonConcatCollection = 'C1233800302-EEDTEST';
-      const query = {
-        concatenate: 'true',
-        maxResults: 2,
-      };
-
-      hookRangesetRequest('1.0.0', nonConcatCollection, 'all', { query, username: 'joe' });
-
-      it('returns a Not Found Error', async function () {
-
-        expect(this.res.statusCode).to.eql(404);
-        expect(this.res.body).to.eql({
-          code: 'harmony.NotFoundError',
-          description: 'Error: no matching service',
-        });
+      StubService.hook({ params: { redirect: 'http://example.com' } });
+      hookRangesetRequest('1.0.0', zarrCollection, 'all', { query });
+      it('sets the concatenate flag on the operation to be true', function () {
+        expect(this.service.operation.shouldConcatenate).to.equal(true);
       });
     });
   });
