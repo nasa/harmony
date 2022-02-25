1f6998908f0e89c236cd9f00a1b4b05a9d2f593e
Hailiang Zhang <hailiang.zhang@nasa.gov>
2022-02-25 16:48:52 -0500

    HARMONY-1063: fix lint issue

--- app/frontends/ogc-coverages/get-coverage-rangeset.ts
+++ app/frontends/ogc-coverages/get-coverage-rangeset.ts
@@ -72,7 +72,7 @@ export default function getCoverageRangeset(
     }
     const point = parsePointParam(query.point);
     if (point) {
-      if (bbox || req.rawHeaders.filter((x) => new RegExp("shapefile=*").test(x)).length)
+      if (bbox || req.rawHeaders.filter((x) => new RegExp('shapefile=*').test(x)).length)
        throw new RequestValidationError('bounding_box and point query parameters should not co-exist');
       operation.spatialPoint = point;
     }
