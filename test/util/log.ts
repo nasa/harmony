8d2c59de83e54ba8b93696213d311710a67d63d4
vinny <vinny.inverso@gmail.com>
2022-03-01 15:09:12 -0500

    HARMONY-1084: Clarify purpose of test

--- test/util/log.ts
+++ test/util/log.ts
@@ -5,7 +5,7 @@ import DataOperation from '../../app/models/data-operation';
 
 describe('util/log', function () {
   describe('redact', function () {
-    it('redacts access tokens from any object', function () {
+    it('redacts access tokens when the token is a top level key', function () {
       const objToRedact = { accessToken: 'token-that-should-be-redacted' };
       redact(objToRedact, [/token/i]);
       expect(objToRedact).to.deep.equal({ accessToken: '<redacted>' });
