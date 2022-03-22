f2cebc6a5c52562ad636e4e7b29b63322377bad0
Chris Durbin <christopher.d.durbin@nasa.gov>
2022-03-22 16:59:58 -0400

    HARMONY-124: Replace C1225996408-POCUMULUS in tests with a collection from the EEDTEST provider that has no variables.

--- test/wms/get-capabilities.ts
+++ test/wms/get-capabilities.ts
@@ -31,25 +31,18 @@ describe('WMS GetCapabilities', function () {
   });
 
   describe('when called on a collection without variable metadata', function () {
-    // Checks below are marked as broken because this collection now contains variables.
-    // With HARMONY-124 we will find a new collection without associated variable metadata
-    // to use for this test.
-    const collection = 'C1225996408-POCUMULUS';
-
+    const collection = 'C1243747507-EEDTEST';
     hookGetCapabilities(collection);
 
-    // Re-enable with HARMONY-124
-    xit('completes successfully', function () {
+    it('completes successfully', function () {
       expect(this.res.status).to.equal(200);
     });
 
-    // Re-enable with HARMONY-124
-    xit('returns a single layer for the entire collection in the capabilities response', function () {
+    it('returns a single layer for the entire collection in the capabilities response', function () {
       expect(this.res.text).to.contain(`<Name>${collection}</Name>`);
     });
 
-    // Re-enable with HARMONY-124
-    xit('does not return any variable layers', function () {
+    it('does not return any variable layers', function () {
       expect(this.res.text).to.not.contain(`<Name>${collection}&#x2F;`);
     });
   });
