9f86c245a90f02662598df2782b189a32d764d5b
vinny <vinny.inverso@gmail.com>
2022-04-01 14:06:23 -0400

    HARMONY-1113: Move filter outside of column

--- public/js/workflow-ui/jobs/jobs-table.js
+++ public/js/workflow-ui/jobs/jobs-table.js
@@ -1,5 +1,23 @@
 import { formatDates, initTooltips } from "../table.js";
-import statusDropdown from "./status-dropdown.js";
+
+function initFilters() {
+  var filterInput = document.querySelector('input[class="jobs-filter"]');
+  new Tagify(filterInput, {
+    whitelist: [
+      "status: successful",
+      "status: canceled",
+      "status: running",
+      "status: failed",
+      "status: accepted"
+    ],
+    maxTags: 20,
+    dropdown: {
+      maxItems: 20,
+      enabled: 0,
+      closeOnSelect: true
+    }
+  })
+}
 
 export default {
 
@@ -9,6 +27,6 @@ export default {
   async init() {
     formatDates();
     initTooltips('[data-bs-toggle="tooltip"]');
-    statusDropdown.init();
+    initFilters();
   }
 }
