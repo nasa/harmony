6abd46c541b6e17926f827b552479d27fa522474
vinny <vinny.inverso@gmail.com>
2022-04-04 13:59:04 -0400

    HARMONY-1113: Change user in test

--- test/workflow-ui/jobs.ts
+++ test/workflow-ui/jobs.ts
@@ -205,11 +205,11 @@ describe('Workflow UI jobs route', function () {
       });
     });
 
-    describe('who filters by an invalid username (w-oody)', function () {
-      hookAdminWorkflowUIJobs({ username: 'adam', jobsFilter: '[{"value":"user: w-oody"}, {"value":"user: woody"}]' });
+    describe('who filters by an invalid username (jo)', function () {
+      hookAdminWorkflowUIJobs({ username: 'adam', jobsFilter: '[{"value":"user: jo"}, {"value":"user: woody"}]' });
       it('ignores the invalid username', function () {
         const listing = this.res.text;
-        expect(listing).to.not.contain('user: w-oody');
+        expect(listing).to.not.contain('user: jo');
         expect(listing).to.contain('user: woody');
       });
     });
