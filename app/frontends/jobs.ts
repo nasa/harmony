6a60dded1025916ea9ea06d2b1dcdd78ca619f39
vinny <vinny.inverso@gmail.com>
2022-04-27 13:29:03 -0400

    HARMONY-1132: Add link for resuming job

--- app/frontends/jobs.ts
+++ app/frontends/jobs.ts
@@ -47,6 +47,13 @@ function getLinksForDisplay(job: Job, urlRoot: string, statusLinkRel: string): J
   if (job.status === JobStatus.SUCCESSFUL && needsStacLink(dataLinks)) {
     links.unshift(new JobLink(getStacCatalogLink(urlRoot, job.jobID)));
   }
+  if (job.status === JobStatus.PAUSED) {
+    links.unshift(new JobLink({
+      title: 'Resumes the job.',
+      href: `${urlRoot}/jobs/${job.jobID}/resume`,
+      type: 'application/json',
+    }));
+  }
   // add a 'self' or 'item' link if it does not already exist
   // 'item' is for use in jobs listings, 'self' for job status
   if (links.filter((link) => link.rel === 'self').length === 0) {
@@ -70,6 +77,9 @@ function getMessageForDisplay(job: Job, urlRoot: string): string {
     }
     message += ' Contains results in AWS S3. Access from AWS '
       + `${env.awsDefaultRegion} with keys from ${urlRoot}/cloud-access.sh`;
+  }
+  if (job.status === JobStatus.PAUSED) {
+    message += '. The job may be resumed using the provided link.';
   }
   return message;
 }
