12b846a0fec72b18e392cac7e0bd282a8af84f5e
vinny <vinny.inverso@gmail.com>
2022-05-10 14:03:16 -0400

    HARMONY-1138: Add toasts

--- public/js/workflow-ui/navLinks.js
+++ public/js/workflow-ui/navLinks.js
@@ -1,7 +1,7 @@
 function buildLinksHtml(links) {
-  const linkToLi = (link) => 
+  const linkToLi = (link) =>
     `<li>
-      <a href="${link.href}" class="nav-link py-0 px-2">
+      <a href="${link.href}" class="state-change-link nav-link py-0 px-2">
         ${link.href.split('/').pop()}
       </a>
     </li>`;
@@ -12,9 +12,24 @@ function buildLinksHtml(links) {
   `;
 }
 
+async function handleClick(event) {
+  event.preventDefault();
+  const link = event.target;
+  const stateChangeUrl = link.getAttribute('href');
+  const res = await fetch(stateChangeUrl);
+  const data = await res.json();
+  console.log(data);
+  if (res.status === 200) {
+    
+  }
+}
+
 function insertLinksHtml(links, linksContainerId) {
   const html = buildLinksHtml(links);
   document.getElementById(linksContainerId).innerHTML = html;
+  document.querySelectorAll('.state-change-link').forEach(function (link) {
+    link.addEventListener('click', handleClick);
+  });
 }
 
 /**
@@ -22,22 +37,22 @@ function insertLinksHtml(links, linksContainerId) {
  * @param
  * @returns
  */
- async function fetchAndInsertLinks(linksContainerId, jobId) {
-    const linksUrl = `./${jobId}/links`;
-    const res = await fetch(linksUrl);
-    if (res.status === 200) {
-      const data = await res.json();
-      insertLinksHtml(data, linksContainerId);
-    }
+async function fetchAndInsertLinks(linksContainerId, jobId) {
+  const linksUrl = `./${jobId}/links`;
+  const res = await fetch(linksUrl);
+  if (res.status === 200) {
+    const data = await res.json();
+    insertLinksHtml(data, linksContainerId);
   }
-  
-  export default {
+}
 
-    /**
-     *
-     * @param
-     */
-    async init(linksContainerId, jobId) {
-      fetchAndInsertLinks(linksContainerId, jobId);
-    }
+export default {
+
+  /**
+   *
+   * @param
+   */
+  async init(linksContainerId, jobId) {
+    fetchAndInsertLinks(linksContainerId, jobId);
   }
+}
