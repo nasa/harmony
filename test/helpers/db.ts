2cfcba4290197f5f89b21f289a38b83fa81118a1
vinny <vinny.inverso@gmail.com>
2022-05-12 12:32:32 -0400

    HARMONY-1138: Clean up test/workflow-ui/job.ts

--- test/helpers/db.ts
+++ test/helpers/db.ts
@@ -6,7 +6,7 @@ import { stub } from 'sinon';
 import util from 'util';
 import db from '../../app/util/db';
 
-const tables = ['jobs', 'work_items', 'workflow_steps', 'job_links'];
+export const tables = ['jobs', 'work_items', 'workflow_steps', 'job_links'];
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const exec = util.promisify(require('child_process').exec);
