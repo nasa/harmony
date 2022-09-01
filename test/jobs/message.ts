a38ddbe1e7e5a75de48dc20f6f63173381c2c351
vinny <vinny.inverso@gmail.com>
2022-09-01 11:32:28 -0400

    HARMONY-1172: Add tests for message handling

---
+++ test/jobs/message.ts
@@ -0,0 +1,144 @@
+import { describe, it } from 'mocha';
+import { buildJob } from '../helpers/jobs';
+import { Job, JobStatus } from '../../app/models/job';
+import { hookTransaction } from '../helpers/db';
+import { expect } from 'chai';
+
+hookTransaction();
+
+describe('skipPreview, pause and resume job message handling', async function () {
+  describe('for a RUNNING job', function () {
+    describe('with a default running message', function () {
+      let job;
+      before(async function () {
+        job = buildJob({ status: JobStatus.RUNNING, message: 'The job is being processed.' });
+        await job.save(this.trx);
+      });
+      it('sets the appropriate message when paused', async function () {
+        job.pause();
+        await job.save(this.trx);
+        const updatedJob = await Job.byJobID(this.trx, job.jobID);
+        expect(updatedJob.message).to.eq('The job is paused and may be resumed using the provided link.');
+      });
+      it('sets the appropriate message when resumed', async function () {
+        job.resume();
+        await job.save(this.trx);
+        const updatedJob = await Job.byJobID(this.trx, job.jobID);
+        expect(updatedJob.message).to.eq('The job is being processed');
+      });
+    });
+    describe('with a results limited running message', function () {
+      let job;
+      let limitedMessage;
+      before(async function () {
+        limitedMessage = 'CMR query identified 10 granules, but the request has been limited.';
+        job = buildJob({ status: JobStatus.RUNNING, message: limitedMessage });
+        await job.save(this.trx);
+      });
+      it('sets the appropriate message when paused', async function () {
+        job.pause();
+        await job.save(this.trx);
+        const updatedJob = await Job.byJobID(this.trx, job.jobID);
+        expect(updatedJob.message).to.eq(`The job is paused and may be resumed using the provided link. ${limitedMessage}`);
+      });
+      it('sets the appropriate message when resumed', async function () {
+        job.resume();
+        await job.save(this.trx);
+        const updatedJob = await Job.byJobID(this.trx, job.jobID);
+        expect(updatedJob.message).to.eq(limitedMessage);
+      });
+    });
+  });
+  describe('for a PREVIEWING job', function () {
+    describe('with a default previewing message', function () {
+      let job;
+      let skipJob;
+      before(async function () {
+        job = buildJob({ status: JobStatus.PREVIEWING, message: 'The job is generating a preview before auto-pausing.' });
+        await job.save(this.trx);
+        skipJob = buildJob({ status: JobStatus.PREVIEWING, message: 'The job is generating a preview before auto-pausing.' });
+        await skipJob.save(this.trx);
+      });
+      describe('which is paused, then resumed', function () {
+        it('sets the appropriate message when paused', async function () {
+          job.pause();
+          await job.save(this.trx);
+          const updatedJob = await Job.byJobID(this.trx, job.jobID);
+          expect(updatedJob.message).to.eq('The job is paused and may be resumed using the provided link.');
+        });
+        it('sets the appropriate message when resumed', async function () {
+          job.resume();
+          await job.save(this.trx);
+          const updatedJob = await Job.byJobID(this.trx, job.jobID);
+          expect(updatedJob.message).to.eq('The job is being processed');
+        });
+      });
+      describe('which skips preview, pauses, and then resumes', function () {
+        it('sets the appropriate message when skipping preview', async function () {
+          skipJob.skipPreview();
+          await skipJob.save(this.trx);
+          const updatedJob = await Job.byJobID(this.trx, skipJob.jobID);
+          expect(updatedJob.message).to.eq('The job is being processed');
+        });
+        it('sets the appropriate message when paused', async function () {
+          skipJob.pause();
+          await skipJob.save(this.trx);
+          const updatedJob = await Job.byJobID(this.trx, skipJob.jobID);
+          expect(updatedJob.message).to.eq('The job is paused and may be resumed using the provided link.');
+        });
+        it('sets the appropriate message when resumed', async function () {
+          skipJob.resume();
+          await skipJob.save(this.trx);
+          const updatedJob = await Job.byJobID(this.trx, skipJob.jobID);
+          expect(updatedJob.message).to.eq('The job is being processed');
+        });
+      });
+    });
+    describe('with a results limited previewing message', function () {
+      let job;
+      let skipJob;
+      let limitedMessage;
+      before(async function () {
+        limitedMessage = 'CMR query identified 10 granules, but the request has been limited.';
+        job = buildJob({ status: JobStatus.PREVIEWING, message: `The job is generating a preview before auto-pausing. ${limitedMessage}` });
+        await job.save(this.trx);
+        skipJob = buildJob({ status: JobStatus.PREVIEWING, message: `The job is generating a preview before auto-pausing. ${limitedMessage}` });
+        await skipJob.save(this.trx);
+      });
+      describe('which is paused, then resumed', function () {
+        it('sets the appropriate message when paused', async function () {
+          job.pause();
+          await job.save(this.trx);
+          const updatedJob = await Job.byJobID(this.trx, job.jobID);
+          expect(updatedJob.message).to.eq(`The job is paused and may be resumed using the provided link. ${limitedMessage}`);
+        });
+        it('sets the appropriate message when resumed', async function () {
+          job.resume();
+          await job.save(this.trx);
+          const updatedJob = await Job.byJobID(this.trx, job.jobID);
+          expect(updatedJob.message).to.eq(limitedMessage);
+        });
+      });
+      describe('which skips preview, pauses, and then resumes', function () {
+        it('sets the appropriate message when skipping preview', async function () {
+          skipJob.skipPreview();
+          await skipJob.save(this.trx);
+          const updatedJob = await Job.byJobID(this.trx, skipJob.jobID);
+          expect(updatedJob.message).to.eq(limitedMessage);
+        });
+        it('sets the appropriate message when paused', async function () {
+          skipJob.pause();
+          await skipJob.save(this.trx);
+          const updatedJob = await Job.byJobID(this.trx, skipJob.jobID);
+          expect(updatedJob.message).to.eq(`The job is paused and may be resumed using the provided link. ${limitedMessage}`);
+        });
+        it('sets the appropriate message when resumed', async function () {
+          skipJob.resume();
+          await skipJob.save(this.trx);
+          const updatedJob = await Job.byJobID(this.trx, skipJob.jobID);
+          expect(updatedJob.message).to.eq(limitedMessage);
+        });
+      });
+    });
+  });
+});
