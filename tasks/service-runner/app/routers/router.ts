919a2ddd92b357b44ca9561ea791dd893fdaeb85
Chris Durbin <christopher.d.durbin@nasa.gov>
2022-10-05 16:29:22 -0400

    HARMONY-1268: Remove unused import.

--- tasks/service-runner/app/routers/router.ts
+++ tasks/service-runner/app/routers/router.ts
@@ -2,7 +2,6 @@ import express, { NextFunction } from 'express';
 import { generateMetricsForPrometheus } from '../service/service-metrics';
 import env from '../util/env';
 import axios from 'axios';
-import logger from '../../../../app/util/log';
 
 /**
  *
