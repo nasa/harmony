b323525b0c543757de98d8d238bd2d2f0ed7be1c
vinny <vinny.inverso@gmail.com>
2022-11-25 14:26:23 -0500

    HARMONY-1323: Remove filter from test

--- test/workflow-ui/work-items-table.ts
+++ test/workflow-ui/work-items-table.ts
@@ -370,8 +370,7 @@ describe('Workflow UI work items table route', function () {
       });
 
       describe('when the admin retrieves work items with a total of four runs', function () {
-        hookWorkflowUIWorkItems({ username: 'adam', jobID: otherJob.jobID, 
-          query: { disallowStatus: 'on', tableFilter: '[{"value":"status: ready","dbValue":"ready","field":"status"}]' } });
+        hookWorkflowUIWorkItems({ username: 'adam', jobID: otherJob.jobID });
         it('returns pod logs links for each runner (pod) of each work item', function () {
           const listing = this.res.text;
           expect((listing.match(/pod-logs-link/g) || []).length).to.equal(4);
