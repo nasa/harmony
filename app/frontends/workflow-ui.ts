5928a6c4a209fce63ec262320e96d6527036b38e
vinny <vinny.inverso@gmail.com>
2022-11-28 11:53:07 -0500

    HARMONY-1323: Maintain consistent ordering

--- app/frontends/workflow-ui.ts
+++ app/frontends/workflow-ui.ts
@@ -271,8 +271,8 @@ function workItemRenderingFunctions(job: Job, isAdmin: boolean, requestUser: str
     workflowItemPodLogs(): string {
       if (!this.runners || this.runners.length < 1) return '';
       return this.runners.map((runner: { id: string, startedAt: number }, i: number) => {
-        const to = this.status === WorkItemStatus.RUNNING ? 'now' : this.updatedAt.toISOString();
         const from = (new Date(runner.startedAt)).toISOString();
+        const to = this.status === WorkItemStatus.RUNNING ? 'now' : this.updatedAt.toISOString();
         const url = `${env.metricsEndpoint}?_g=(filters:!(),refreshInterval:(pause:!t,value:0),` +
           `time:(from:'${from}',to:'${to}'))` +
           `&_a=(columns:!(),filters:!(),index:${env.metricsIndex},interval:auto,` +
