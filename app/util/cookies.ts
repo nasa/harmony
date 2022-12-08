8b8a399382c72bc92a9b01aaab6e6b6433d13d44
vinny <vinny.inverso@gmail.com>
2022-12-08 08:29:49 -0500

    HARMONY-1269: Audit fixes

--- app/util/cookies.ts
+++ app/util/cookies.ts
@@ -1,4 +1,4 @@
-import { Response } from 'express';
+import { CookieOptions, Response } from 'express';
 import { mergeParameters } from './parameter-parsing';
 import * as urlUtil from './url';
 import HarmonyRequest from '../models/harmony-request';
@@ -16,7 +16,7 @@ interface FileParams {
  * containing the cookie name, value, and options.
  */
 
-export const cookieOptions = { signed: true, sameSite: 'Lax' };
+export const cookieOptions: CookieOptions = { signed: true, sameSite: 'lax' };
 
 /**
  * Recipe for a cookie to support handling shapefiles
