6d534c31c5b31510218b65518dcb5a0f55cf12a9
James Norton <james.norton@nasa.gov>
2023-02-01 14:36:36 -0500

    HARMONY-1342: Add multi-value test for granuleName parameter

--- test/parameters/granule_name.ts
+++ test/parameters/granule_name.ts
@@ -4,21 +4,16 @@ import { hookRedirect } from '../helpers/hooks';
 import { hookRangesetRequest } from '../helpers/ogc-api-coverages';
 import hookServersStartStop from '../helpers/servers';
 
-const regularQuery = {
-  format: 'application/x-zarr',
-  granuleName: '001_08_7f00ff_oceania_east',
-};
-
-const wildCardQuery = {
-  format: 'application/x-zarr',
-  granuleName: '001_*',
-};
-
 describe('when passing the granuleName parameter', function () {
   const collection = 'C1233800302-EEDTEST';
   hookServersStartStop();
 
   describe('when making a request with a valid granuleName without wildcards', function () {
+    const regularQuery = {
+      format: 'application/x-zarr',
+      granuleName: '001_08_7f00ff_oceania_east',
+    };
+
     hookRangesetRequest('1.0.0', collection, 'all', { query: { ...regularQuery } });
     hookRedirect('anonymous');
     hookTransaction();
@@ -35,6 +30,11 @@ describe('when passing the granuleName parameter', function () {
   // wildcards are tested more thoroughly in the cmr tests - this is just to make sure it
   // works with more than one result
   describe('when making a request with a valid granuleName with wildcards', function () {
+    const wildCardQuery = {
+      format: 'application/x-zarr',
+      granuleName: '001_*',
+    };
+
     hookRangesetRequest('1.0.0', collection, 'all', { query: { ...wildCardQuery } });
     hookRedirect('anonymous');
     hookTransaction();
@@ -45,6 +45,25 @@ describe('when passing the granuleName parameter', function () {
 
     it('processes all the matching granules', async function () {
       expect(this.res.body.numInputGranules).to.equal(12);
+    });
+  });
+
+  describe('when making a request with multiple valid granuleName values', function () {
+    const multiValueQuery = {
+      format: 'application/x-zarr',
+      granuleName: ['001_08_7f00ff_oceania_east', '001_03_7f00ff_asia_east'],
+    };
+
+    hookRangesetRequest('1.0.0', collection, 'all', { query: { ...multiValueQuery } });
+    hookRedirect('anonymous');
+    hookTransaction();
+
+    it('returns a 200 status code for the request', async function () {
+      expect(this.res.status).to.equal(200);
+    });
+
+    it('processes all the matching granules', async function () {
+      expect(this.res.body.numInputGranules).to.equal(2);
     });
   });
 });
