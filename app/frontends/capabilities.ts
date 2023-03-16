4236f347b0e1b84797cf6552b396564cc3a5f6dd
Chris Durbin <christopher.d.durbin@nasa.gov>
2023-03-16 13:35:05 -0400

    HARMONY-1386: Refactor collection capabilities into JSON and HTML views.

--- app/frontends/capabilities.ts
+++ app/frontends/capabilities.ts
@@ -5,6 +5,7 @@ import { RequestValidationError } from '../util/errors';
 import { CmrCollection, getCollectionsByIds, getVariablesForCollection } from '../util/cmr';
 import { addCollectionsToServicesByAssociation } from '../middleware/service-selection';
 import _ from 'lodash';
+import { ServiceConfig } from '../models/services/base-service';
 
 /**
  * Loads the collection info from CMR
@@ -26,44 +27,100 @@ async function loadCollectionInfo(collectionId: string, token: string): Promise<
   return collection;
 }
 
+interface CollectionCapabilities {
+  conceptId: string;
+  shortName: string;
+  variableSubset: boolean;
+  bboxSubset: boolean;
+  shapefileSubset: boolean;
+  concatenate: boolean;
+  reproject: boolean;
+  outputFormats: string[];
+  services: ServiceConfig<unknown>[];
+  variables: string[];
+}
+
 /**
- * Return job state change links so that the user can pause, resume, cancel, etc., a job.
+ * Resolves to a CollectionCapabilities object detailing the harmony transformation capabilities
+ * for the given collection.
+ *
+ * @param collectionId - the CMR collection concept ID
+ * @param cmrCollections - a list of CMR collections relevant to the user request
+ * @param token - the user's EDL access token
+ * @returns a promise resolving to the collection capabilities
+ */
+async function getCollectionCapabilities(
+  collectionId: string, token: string, cmrCollections: CmrCollection[],
+) : Promise<CollectionCapabilities> {
+  const collection = await loadCollectionInfo(collectionId, token);
+  const allServiceConfigs = addCollectionsToServicesByAssociation(cmrCollections);
+  const matchingServices = allServiceConfigs.filter((config) =>
+    config.collections.map((c) => c.id).includes(collection.id));
+  const variables = collection.variables.map((v) => v.umm.Name);
+  const variableSubset = variables.length > 0
+      && matchingServices.some((s) => s.capabilities.subsetting.variable === true);
+  const bboxSubset = matchingServices.some((s) => s.capabilities.subsetting.bbox === true);
+  const shapefileSubset = matchingServices.some((s) => s.capabilities.subsetting.shape === true);
+  const concatenate = matchingServices.some((s) => s.capabilities.concatenation === true);
+  const reproject = matchingServices.some((s) => s.capabilities.reprojection === true);
+  const outputFormats = new Set(matchingServices.flatMap((s) => s.capabilities.output_formats));
+  const conceptId = collection.id;
+  const shortName = collection.short_name;
+  const services = matchingServices.map((s) => _.pick(s, ['name', 'capabilities']));
+  const capabilities = { conceptId, shortName, variableSubset, bboxSubset, shapefileSubset,
+    concatenate, reproject, outputFormats: Array.from(outputFormats), services, variables,
+  };
+  return capabilities;
+}
+
+/**
+ * Endpoint to display information related to what harmony operations are supported for a
+ * given collection in JSON format.
  *
  * @param req - The request sent by the client
  * @param res - The response to send to the client
  * @param next - The next function in the call chain
  * @returns The job links (pause, resume, etc.)
  */
-export async function getCollectionCapabilities(
+export async function displayCollectionCapabilities(
   req: HarmonyRequest, res: Response, next: NextFunction,
 ): Promise<void> {
-  // try {
   const query = keysToLowerCase(req.query);
   const { collectionid } = query;
   if (!collectionid) {
     throw new RequestValidationError('Missing required parameter collectionId');
   } try {
-    const collection = await loadCollectionInfo(collectionid, req.accessToken);
-    const allServiceChainConfigs = addCollectionsToServicesByAssociation(req.collections);
-    const matchingServiceChains = allServiceChainConfigs.filter((config) =>
-      config.collections.map((c) => c.id).includes(collection.id));
-    // const variables = collection.variables.map((v) => v.umm.Name);
-    const variables = [];
-    const variableSubset = variables.length > 0
-      && matchingServiceChains.some((s) => s.capabilities.subsetting.variable === true);
-    const bboxSubset = matchingServiceChains.some((s) => s.capabilities.subsetting.bbox === true);
-    const shapefileSubset = matchingServiceChains.some((s) => s.capabilities.subsetting.shape === true);
-    const concatenate = matchingServiceChains.some((s) => s.capabilities.concatenation === true);
-    const reproject = matchingServiceChains.some((s) => s.capabilities.reprojection === true);
-    const outputFormats = new Set(matchingServiceChains.flatMap((s) => s.capabilities.output_formats));
-    const conceptId = collection.id;
-    const shortName = collection.short_name;
-    const serviceChains = matchingServiceChains.map((s) => _.pick(s, ['name', 'capabilities']));
-    // const capabilities = { 'conceptId': collectionid };
-    const capabilities = { conceptId, shortName, variableSubset, bboxSubset, shapefileSubset,
-      concatenate, reproject, outputFormats: Array.from(outputFormats), serviceChains, variables,
-    };
+    const capabilities = await getCollectionCapabilities(
+      collectionid, req.accessToken, req.collections,
+    );
     res.send(capabilities);
+  } catch (e) {
+    req.context.logger.error(e);
+    next(e);
+  }
+}
+
+/**
+ * Endpoint to display information related to what harmony operations are supported for a
+ * given collection in HTML format.
+ *
+ * @param req - The request sent by the client
+ * @param res - The response to send to the client
+ * @param next - The next function in the call chain
+ * @returns The job links (pause, resume, etc.)
+ */
+export async function displayCollectionCapabilitiesHtml(
+  req: HarmonyRequest, res: Response, next: NextFunction,
+): Promise<void> {
+  const query = keysToLowerCase(req.query);
+  const { collectionid } = query;
+  if (!collectionid) {
+    throw new RequestValidationError('Missing required parameter collectionId');
+  } try {
+    const capabilities = await getCollectionCapabilities(
+      collectionid, req.accessToken, req.collections,
+    );
+    res.render('capabilities/index', { capabilities });
   } catch (e) {
     req.context.logger.error(e);
     next(e);
