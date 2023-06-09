07196c610df7d0fb39a921ce84dee91adaa5ba17
Yonggang Liu <ygliuvt@gmail.com>
2023-06-09 10:37:37 -0400

    HARMONY-1397: As a harmony user, I want collection capabilities to include a link to the CMR UMM-Var record for each variable.

--- app/frontends/capabilities.ts
+++ app/frontends/capabilities.ts
@@ -2,15 +2,20 @@ import HarmonyRequest from '../models/harmony-request';
 import { Response, NextFunction } from 'express';
 import { keysToLowerCase } from '../util/object';
 import { NotFoundError, RequestValidationError } from '../util/errors';
-import { CmrCollection, getCollectionsByIds, getCollectionsByShortName, getVariablesForCollection } from '../util/cmr';
+import { CmrCollection, getCollectionsByIds, getCollectionsByShortName, getVariablesForCollection, CmrUmmVariable } from '../util/cmr';
 import { addCollectionsToServicesByAssociation } from '../middleware/service-selection';
 import _ from 'lodash';
 import { ServiceConfig } from '../models/services/base-service';
 import { harmonyCollections } from '../models/services';
 import { listToText } from '../util/string';
 
-const currentApiVersion = '1';
-const supportedApiVersions = ['1'];
+export const currentApiVersion = '2';
+const supportedApiVersions = ['1', '2'];
+
+interface VariableV2 {
+  name: string;
+  href: string;
+}
 
 interface CollectionCapabilitiesV1 {
   conceptId: string;
@@ -23,9 +28,24 @@ interface CollectionCapabilitiesV1 {
   outputFormats: string[];
   services: ServiceConfig<unknown>[];
   variables: string[];
+  capabilitiesVersion: string;
 }
 
-type CollectionCapabilities = CollectionCapabilitiesV1;
+interface CollectionCapabilitiesV2 {
+  conceptId: string;
+  shortName: string;
+  variableSubset: boolean;
+  bboxSubset: boolean;
+  shapeSubset: boolean;
+  concatenate: boolean;
+  reproject: boolean;
+  outputFormats: string[];
+  services: ServiceConfig<unknown>[];
+  variables: VariableV2[];
+  capabilitiesVersion: string;
+}
+
+type CollectionCapabilities = CollectionCapabilitiesV1 | CollectionCapabilitiesV2;
 
 /**
  * Loads the collection info from CMR using short name or concept ID passed into the harmony
@@ -48,8 +68,8 @@ async function loadCollectionInfo(req: HarmonyRequest): Promise<CmrCollection> {
     collections = await getCollectionsByIds([collectionid], req.accessToken);
     if (collections.length === 0) {
       const message = `${collectionid} must be a CMR collection identifier, but `
-      + 'we could not find a matching collection. Please make sure the collection ID '
-      + 'is correct and that you have access to it.';
+        + 'we could not find a matching collection. Please make sure the collection ID '
+        + 'is correct and that you have access to it.';
       throw new NotFoundError(message);
     }
     pickedCollection = collections[0];
@@ -57,7 +77,7 @@ async function loadCollectionInfo(req: HarmonyRequest): Promise<CmrCollection> {
     collections = await getCollectionsByShortName(shortname, req.accessToken);
     if (collections.length === 0) {
       const message = `Unable to find collection short name ${shortname} in the CMR. Please `
-      + ' make sure the short name is correct and that you have access to the collection.';
+        + ' make sure the short name is correct and that you have access to the collection.';
       throw new NotFoundError(message);
     }
     pickedCollection = collections[0];
@@ -73,6 +93,18 @@ async function loadCollectionInfo(req: HarmonyRequest): Promise<CmrCollection> {
 }
 
 /**
+ * Returns the variables representation in capabilities version 2 format.
+ *
+ * @param variable - the CMR umm-var object
+ * @returns the variables representation in capabilities version 2 format
+ */
+function getVariableV2(variable: CmrUmmVariable): VariableV2 {
+  const name = variable.umm.Name;
+  const href = `${process.env.CMR_ENDPOINT}/search/concepts/${variable.meta['concept-id']}`;
+  return { name, href };
+}
+
+/**
  * Resolves to a CollectionCapabilitiesV1 object detailing the harmony transformation capabilities
  * for the given collection in version 1 of the JSON format.
  *
@@ -81,12 +113,13 @@ async function loadCollectionInfo(req: HarmonyRequest): Promise<CmrCollection> {
  */
 async function getCollectionCapabilitiesV1(collection: CmrCollection)
   : Promise<CollectionCapabilitiesV1> {
+  const capabilitiesVersion = '1';
   const allServiceConfigs = addCollectionsToServicesByAssociation([collection]);
   const matchingServices = allServiceConfigs.filter((config) =>
     config.collections.map((c) => c.id).includes(collection.id));
   const variables = collection.variables.map((v) => v.umm.Name);
   const variableSubset = variables.length > 0
-     && matchingServices.some((s) => s.capabilities.subsetting.variable === true);
+    && matchingServices.some((s) => s.capabilities.subsetting.variable === true);
   const bboxSubset = matchingServices.some((s) => s.capabilities.subsetting.bbox === true);
   const shapeSubset = matchingServices.some((s) => s.capabilities.subsetting.shape === true);
   const concatenate = matchingServices.some((s) => s.capabilities.concatenation === true);
@@ -95,8 +128,40 @@ async function getCollectionCapabilitiesV1(collection: CmrCollection)
   const conceptId = collection.id;
   const shortName = collection.short_name;
   const services = matchingServices.map((s) => _.pick(s, ['name', 'capabilities']));
-  const capabilities = { conceptId, shortName, variableSubset, bboxSubset, shapeSubset,
-    concatenate, reproject, outputFormats: Array.from(outputFormats), services, variables,
+  const capabilities = {
+    conceptId, shortName, variableSubset, bboxSubset, shapeSubset,
+    concatenate, reproject, outputFormats: Array.from(outputFormats), services, variables, capabilitiesVersion,
+  };
+  return capabilities;
+}
+
+/**
+ * Resolves to a CollectionCapabilitiesV2 object detailing the harmony transformation capabilities
+ * for the given collection in version 2 of the JSON format.
+ *
+ * @param collection - the CMR collection
+ * @returns a promise resolving to the version 2 collection capabilities
+ */
+async function getCollectionCapabilitiesV2(collection: CmrCollection)
+  : Promise<CollectionCapabilitiesV2> {
+  const capabilitiesVersion = '2';
+  const allServiceConfigs = addCollectionsToServicesByAssociation([collection]);
+  const matchingServices = allServiceConfigs.filter((config) =>
+    config.collections.map((c) => c.id).includes(collection.id));
+  const variables = collection.variables.map((v) => getVariableV2(v));
+  const variableSubset = variables.length > 0
+    && matchingServices.some((s) => s.capabilities.subsetting.variable === true);
+  const bboxSubset = matchingServices.some((s) => s.capabilities.subsetting.bbox === true);
+  const shapeSubset = matchingServices.some((s) => s.capabilities.subsetting.shape === true);
+  const concatenate = matchingServices.some((s) => s.capabilities.concatenation === true);
+  const reproject = matchingServices.some((s) => s.capabilities.reprojection === true);
+  const outputFormats = new Set(matchingServices.flatMap((s) => s.capabilities.output_formats));
+  const conceptId = collection.id;
+  const shortName = collection.short_name;
+  const services = matchingServices.map((s) => _.pick(s, ['name', 'capabilities']));
+  const capabilities = {
+    conceptId, shortName, variableSubset, bboxSubset, shapeSubset,
+    concatenate, reproject, outputFormats: Array.from(outputFormats), services, variables, capabilitiesVersion,
   };
   return capabilities;
 }
@@ -112,10 +177,11 @@ async function getCollectionCapabilitiesV1(collection: CmrCollection)
 function chooseCapabilitiesFunction(version: string): ((string) => Promise<CollectionCapabilities>) {
   if (version === '1') {
     return getCollectionCapabilitiesV1;
+  } else if (version === '2') {
+    return getCollectionCapabilitiesV2;
   }
 
-  const message = `Invalid API version ${version}, supported versions: ` +
-    + listToText(supportedApiVersions);
+  const message = `Invalid API version ${version}, supported versions: ${listToText(supportedApiVersions)}`;
   throw new RequestValidationError(message);
 }
 
