58b8652b2be7f2f6253308c616efd651cfc23755
vinny <vinny.inverso@gmail.com>
2023-07-14 16:40:10 -0400

    HARMONY-1495: Set up collections for tests

--- test/eula-acceptance.ts
+++ test/eula-acceptance.ts
@@ -12,7 +12,14 @@ import { stub } from 'sinon';
 import env from '../app/util/env';
 import { hookTransactionFailure } from './helpers/db';
 
-describe('OGC API Coverages - getCoverageRangeset EULA acceptance', function () {
+describe('EULA acceptance validation', function () {
+
+  // Whether or not a collection has a EULA as implied by these variable names
+  // depends on whether EULA identifiers are present in the collection's metadata
+  const noEulasCollection = 'C1233800302-EEDTEST';
+  const twoEulasCollection = 'C1258836670-EEDTEST';
+  const oneEulaCollection = 'C1258839703-EEDTEST';
+  const badEulaIdCollection = 'C1258840703-EEDTEST';
 
   const query = {
     format: 'image/png',
@@ -23,7 +30,7 @@ describe('OGC API Coverages - getCoverageRangeset EULA acceptance', function () 
 
   describe('which has 2 unaccepted EULAS', function () {
     StubService.hook({ params: { redirect: 'http://example.com' } });
-    hookPostRangesetRequest(
+    hookRangesetRequest(
       '1.0.0',
       collection,
       'red_var',
