a04b75587531c2bffd38b7c51021e06f873c3f1f
James Norton <james.norton@nasa.gov>
2023-08-10 15:55:30 -0400

    HARMONY-1537: Fix broken tests

--- kubernetes-services/work-updater/test/helpers/env.ts
+++ kubernetes-services/work-updater/test/helpers/env.ts
@@ -2,3 +2,9 @@ process.env.NODE_ENV = 'test';
 // work-around to pass check in app/util/env.ts - should be refactored
 process.env.SHARED_SECRET_KEY = 'foo';
 process.env.PORT = '5000';
+// We do not use an EDL application or call backend services in our tests.
+process.env.COOKIE_SECRET = 'foo';
+process.env.OAUTH_CLIENT_ID = 'foo';
+process.env.OAUTH_UID = 'foo';
+process.env.OAUTH_PASSWORD = 'foo';
+process.env.SHARED_SECRET_KEY = 'foo';
