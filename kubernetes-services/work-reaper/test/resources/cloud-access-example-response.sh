1864582cf323d9219e734f8d7a680336828479d8
James Norton <james.norton@nasa.gov>
2023-09-11 15:31:10 -0400

    HARMONY-1509: Fix broken tests

--- kubernetes-services/work-reaper/test/resources/cloud-access-example-response.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Source this file to set keys to access Harmony S3 outputs within the us-west-2 region.
-# Keys will expire on Fri, 10 Apr 2020 18:03:46 GMT
-
-export AWS_ACCESS_KEY_ID='XXXXXXXXXXXXXXXXXXXX'
-export AWS_SECRET_ACCESS_KEY='XXXXXXXXXXXXXXXXXXXX1111111111+++++/////'
-export AWS_SESSION_TOKEN='XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa++++++++++++++++++++++++++++++++++++++++++++++++++00000000000000000000000000000000000000000000000000//////////////////////////////////////////////////XXXXXX=================================================='
