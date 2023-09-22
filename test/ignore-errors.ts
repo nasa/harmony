8099d79ae759fe39f4cd38118c3fdf8aaeb72742
vinny <vinny.inverso@gmail.com>
2023-09-22 10:58:05 -0400

    HARMONY-945: Update test to match new message

--- test/ignore-errors.ts
+++ test/ignore-errors.ts
@@ -268,7 +268,7 @@ describe('ignoreErrors', function () {
           // all work items failing should trigger job failure
           const job = await Job.byJobID(db, secondSwotItem.jobID);
           expect(job.status).to.equal(JobStatus.FAILED);
-          expect(job.message).to.equal('The job has failed with 2 errors. See the errors field for more details');
+          expect(job.message).to.equal('The job failed with 2 errors. See the errors field for more details');
           const currentWorkItems = (await getWorkItemsByJobId(db, job.jobID)).workItems;
           expect(currentWorkItems.length).to.equal(4);
           expect(currentWorkItems.filter((item) => item.status === WorkItemStatus.SUCCESSFUL && item.serviceID === 'harmonyservices/query-cmr:latest').length).to.equal(1);
