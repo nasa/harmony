9c7f471b362eb8aa6f0c47dbabb64b0bbba3889b
vinny <vinny.inverso@gmail.com>
2023-10-11 14:02:29 -0400

    HARMONY-398: Test using a service filter

--- test/workflow-ui/job-table-rows.ts
+++ test/workflow-ui/job-table-rows.ts
@@ -5,23 +5,29 @@ import hookServersStartStop from '../helpers/servers';
 import { hookTransaction } from '../helpers/db';
 import { buildJob } from '../helpers/jobs';
 import { hookWorkflowUIJobRows } from '../helpers/workflow-ui';
+import * as sinon from 'sinon';
+import * as services from '../../app/models/services';
 
 
 // main objects used in the tests
 const boJob1 = buildJob({ status: JobStatus.FAILED, username: 'bo' });
-const boJob2 = buildJob({ status: JobStatus.SUCCESSFUL, username: 'bo' });
+const boJob2 = buildJob({ status: JobStatus.SUCCESSFUL, username: 'bo', service_name: 'cog-maker' });
 const adamJob1 = buildJob({ status: JobStatus.RUNNING, username: 'adam' });
-
 
 describe('Workflow UI job table rows route', function () {
   hookServersStartStop({ skipEarthdataLogin: false });
 
   hookTransaction();
+  let servicesStub: sinon.SinonStub;
   before(async function () {
+    servicesStub = sinon.stub(services, 'serviceNames').value(['cog-maker', 'netcdf-to-zarr']);
     await boJob1.save(this.trx);
     await boJob2.save(this.trx);
     await adamJob1.save(this.trx);
     this.trx.commit();
+  });
+  after(function () {
+    servicesStub.restore();
   });
 
   describe('for an invalid job ID format', function () {
@@ -38,6 +44,17 @@ describe('Workflow UI job table rows route', function () {
   describe('who requests their SUCCESSFUL jobs', function () {
     hookWorkflowUIJobRows({ username: 'bo', jobIDs: [boJob1.jobID, boJob2.jobID], query: { tableFilter: '[{"value":"status: successful","dbValue":"successful","field":"status"}]' } });
     it('returns only the successful job row', function () {
+      const response = JSON.parse(this.res.text);
+      expect(response[boJob1.jobID]).to.eq(undefined);
+      expect(response[boJob2.jobID]).contains(`<tr id="job-${boJob2.jobID}" class='job-table-row'>`);
+      expect(Object.keys(response).length).to.eq(1);
+    });
+  });
+
+  describe('who uses a service name filter', function () {
+    hookWorkflowUIJobRows({ username: 'bo', jobIDs: [boJob1.jobID, boJob2.jobID],
+      query: { disallowService: false, tableFilter: '[{"value":"service: cog-maker","dbValue":"cog-maker","field":"service"}]' } });
+    it('returns only the job row for the cog-maker service job', function () {
       const response = JSON.parse(this.res.text);
       expect(response[boJob1.jobID]).to.eq(undefined);
       expect(response[boJob2.jobID]).contains(`<tr id="job-${boJob2.jobID}" class='job-table-row'>`);
