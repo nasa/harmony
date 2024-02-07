d3e0ef27edcb1194d04634bd4cd070dfebd8029f
vinny <vinny.inverso@gmail.com>
2024-02-07 13:40:20 -0500

    HARMONY-1677: Stub the edl token endpoint

--- services/harmony/test/util/edl-api.ts
+++ services/harmony/test/util/edl-api.ts
@@ -4,18 +4,27 @@ import {
   getEdlGroupInformation,
 } from '../../app/util/edl-api';
 import logger from '../../app/util/log';
+import { stubEdlRequest, token } from 'test/helpers/auth';
+
+before(function() {
+  stubEdlRequest(
+    '/oauth/token',
+    { grant_type: 'client_credentials' },
+    token({ accessToken: 'fake_access' }),
+  );
+})
 
 describe('util/edl-api', function () {
   describe('getEdlGroupInformation', function () {
     describe('when the user is not part of the service deployers group', function () {
       it('returns isServiceDeployer:false', async function () {
-        const groups = await getEdlGroupInformation('woody', logger);
+        const groups = await getEdlGroupInformation('joe', logger);
         expect(groups.isServiceDeployer).is.false;
       });
     });
     describe('when the user is part of the service deployers group', function () {
       it('returns isServiceDeployer:true', async function () {
-        const groups = await getEdlGroupInformation('tim', logger);
+        const groups = await getEdlGroupInformation('eve', logger);
         expect(groups.isServiceDeployer).is.true;
       });
     });
