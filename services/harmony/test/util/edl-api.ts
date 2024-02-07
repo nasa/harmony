b3f1a745b6fe6b953b77d4ff8bfb091a244ba0f2
vinny <vinny.inverso@gmail.com>
2024-02-07 16:04:24 -0500

    HARMONY-1677: Fix invocation point for hooks

--- services/harmony/test/util/edl-api.ts
+++ services/harmony/test/util/edl-api.ts
@@ -6,20 +6,18 @@ import {
 import logger from '../../app/util/log';
 import { stubEdlRequest, token, unstubEdlRequest } from '../helpers/auth';
 
-before(function () {
-  stubEdlRequest(
-    '/oauth/token',
-    { grant_type: 'client_credentials' },
-    token({ accessToken: 'fake_access' }),
-  );
-});
-
-after(function () {
-  unstubEdlRequest();
-});
-
 describe('util/edl-api', function () {
   describe('getEdlGroupInformation', function () {
+    before(function () {
+      stubEdlRequest(
+        '/oauth/token',
+        { grant_type: 'client_credentials' },
+        token({ accessToken: 'fake_access' }),
+      );
+    });
+    after(function () {
+      unstubEdlRequest();
+    });
     describe('when the user is not part of the service deployers group', function () {
       it('returns isServiceDeployer:false', async function () {
         const groups = await getEdlGroupInformation('joe', logger);
