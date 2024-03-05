89dfdb32e606f6e8eb3ea33bf83aa003550e455e
James Norton <jamesnorton@gmail.com>
2024-03-05 13:31:04 -0500

    Harmony 1682 (#541)
    * Add check for reachability for service image/tag and refactor image/tag validations
    
    ---------
    
    Co-authored-by: Chris Durbin <christopher.d.durbin@nasa.gov>

--- services/harmony/test/service-image-tags.ts
+++ services/harmony/test/service-image-tags.ts
@@ -6,6 +6,8 @@ import hookServersStartStop from './helpers/servers';
 import { hookRedirect } from './helpers/hooks';
 import { auth } from './helpers/auth';
 import * as serviceImageTags from '../app/frontends/service-image-tags';
+import { checkServiceExists, checkTag, getImageTagMap, ecrImageNameToComponents } from '../app/frontends/service-image-tags';
+import hookDescribeImage from './helpers/container-registry';
 
 //
 // Tests for the service-image endpoint
@@ -42,6 +44,144 @@ const errorMsg404 = 'Service foo does not exist.\nThe existing services and thei
 const userErrorMsg = 'User joe is not in the service deployers or admin EDL groups';
 
 const tagContentErrorMsg = 'A tag name may contain lowercase and uppercase characters, digits, underscores, periods and dashes. A tag name may not start with a period or a dash and may contain a maximum of 128 characters.';
+
+//
+// Unit tests
+//
+
+describe('getImageTagMap', function () {
+  let originalEnv: NodeJS.ProcessEnv;
+
+  beforeEach(function () {
+    // Save the original process.env
+    originalEnv = process.env;
+
+    // Mock process.env for our tests
+    process.env = {};
+  });
+
+  afterEach(function () {
+    // Restore the original process.env after each test
+    process.env = originalEnv;
+  });
+
+  it('should correctly map service names to image tags, excluding Harmony core services', function () {
+    // Setup
+    process.env.MY_SERVICE_IMAGE = 'repo/my-service:latest';
+    process.env.ANOTHER_SERVICE_IMAGE = 'repo/another-service:v1.2.3';
+    process.env.WORK_FAILER_IMAGE = 'harmonyservices/work-failer:latest';
+    process.env.MISSING_TAG_IMAGE = 'repo/missing-tag-service';
+
+    const result = getImageTagMap();
+
+    expect(result).to.be.an('object');
+    expect(result).to.have.property('my-service', 'latest');
+    expect(result).to.have.property('another-service', 'v1.2.3');
+    expect(result).not.to.have.property('work-failer');
+    expect(result).not.to.have.property('missing-tag-service');
+  });
+});
+
+describe('checkServiceExists', function () {
+  let originalEnv: NodeJS.ProcessEnv;
+
+  beforeEach(function () {
+    // Save the original process.env
+    originalEnv = process.env;
+
+    // Mock process.env for our tests
+    process.env = {};
+    // Setup
+    process.env.MY_SERVICE_IMAGE = 'repo/my-service:latest';
+    process.env.ANOTHER_SERVICE_IMAGE = 'repo/another-service:v1.2.3';
+    process.env.WORK_FAILER_IMAGE = 'harmonyservices/work-failer:latest';
+    process.env.MISSING_TAG_IMAGE = 'repo/missing-tag-service';
+  });
+
+  afterEach(function () {
+    // Restore the original process.env after each test
+    process.env = originalEnv;
+  });
+
+  it('should return null if the service exists', function () {
+    const result = checkServiceExists('my-service');
+    expect(result).to.be.null;
+  });
+
+  it('should return an error message if the service does not exist', function () {
+    const result = checkServiceExists('foo');
+
+    expect(result).to.include('Service foo does not exist.');
+    expect(result).to.include('The existing services and their images are');
+    expect(result).to.include(JSON.stringify(getImageTagMap(), null, 2));
+  });
+});
+
+describe('checkTag', function () {
+  it('should return null for valid tags', function () {
+    // Examples of valid tags
+    const validTags = [
+      'latest',
+      '1.0',
+      'v1.0.1',
+      'version_1.2.3',
+      'a'.repeat(128), // Maximum length
+    ];
+
+    validTags.forEach(tag => {
+      const result = checkTag(tag);
+      expect(result).to.be.null;
+    });
+  });
+
+  it('should return an error message for invalid tags', function () {
+    // Examples of invalid tags
+    const invalidTags = [
+      '.startwithdot',
+      '-startwithdash',
+      '!invalidchar',
+      'a'.repeat(129), // Exceeds maximum length
+    ];
+
+    const errorMessage = 'A tag name may contain lowercase and uppercase characters, digits, underscores, periods and dashes. A tag name may not start with a period or a dash and may contain a maximum of 128 characters.';
+
+    invalidTags.forEach(tag => {
+      const result = checkTag(tag);
+      expect(result).to.equal(errorMessage);
+    });
+  });
+});
+
+describe('ecrImageNameToComponents', function () {
+  it('should correctly break down a valid ECR image name into its components', function () {
+    // Example of a valid ECR image name
+    const imageName = '123456789012.dkr.ecr.us-west-2.amazonaws.com/harmony/my-repository:my-tag';
+
+    const expectedComponents = {
+      host: '123456789012.dkr.ecr.us-west-2.amazonaws.com',
+      region: 'us-west-2',
+      repository: 'harmony/my-repository',
+      tag: 'my-tag',
+    };
+
+    const components = ecrImageNameToComponents(imageName);
+
+    expect(components).to.deep.equal(expectedComponents);
+  });
+
+  it('should return null for an invalid ECR image name', function () {
+    // Example of an invalid ECR image name
+    const invalidImageName = 'invalid-image-name';
+
+    const components = ecrImageNameToComponents(invalidImageName);
+
+    expect(components).to.be.null;
+  });
+});
+
+//
+// Integration tests
+//
 
 describe('Service image endpoint', async function () {
   hookServersStartStop({ skipEarthdataLogin: false });
@@ -269,7 +409,7 @@ describe('Service image endpoint', async function () {
 
       before(async function () {
         hookRedirect('buzz');
-        this.res = await request(this.frontend).put('/service-image-tag/foo').use(auth({ username: 'buzz' }));
+        this.res = await request(this.frontend).put('/service-image-tag/foo').use(auth({ username: 'buzz' })).send({ tag: 'foo' });
       });
 
       after(function () {
@@ -325,13 +465,83 @@ describe('Service image endpoint', async function () {
       });
     });
 
-    describe('when the user is in the deployers group and a valid tag is sent in the request', async function () {
+    describe('when the user is in the deployers group, but the image is not reachable', async function () {
+      let originalEnv;
       before(async function () {
         hookRedirect('buzz');
-        this.res = await request(this.frontend).put('/service-image-tag/harmony-service-example').use(auth({ username: 'buzz' })).send({ tag: 'foo' });
+        // Save the original process.env
+        originalEnv = process.env;
+
+        // Setup
+        process.env.MY_ECR_SERVICE_IMAGE = '123456789012.dkr.ecr.us-west-2.amazonaws.com/harmony/my-repository:my-tag';
+        process.env.MY_GHCR_SERVICE_IMAGE = 'ghcr.io/nasa/my-repository:my-tag';
       });
 
       after(function () {
+        // Restore the original process.env after each test
+        process.env = originalEnv;
+      });
+
+      describe('when the image is an ECR image', async function () {
+        hookDescribeImage(null);
+
+        before(async function () {
+          this.res = await request(this.frontend).put('/service-image-tag/my-ecr-service').use(auth({ username: 'buzz' })).send({ tag: 'foo' });
+        });
+
+        after(function () {
+          delete this.res;
+        });
+
+        it('returns a status 404', async function () {
+          expect(this.res.status).to.equal(404);
+        });
+
+        it('returns a meaningful error message', async function () {
+          expect(this.res.text).to.equal('123456789012.dkr.ecr.us-west-2.amazonaws.com/harmony/my-repository:foo is unreachable');
+        });
+      });
+
+      describe('when the image is not an ECR image', async function () {
+        let execStub;
+        hookDescribeImage(null);
+
+        before(async function () {
+          // resolve to non-zero exit code meaning script failed
+          execStub = sinon.stub(serviceImageTags, 'asyncExec').callsFake(() => Promise.resolve({ err: { code: 1 } }));
+          this.res = await request(this.frontend).put('/service-image-tag/my-ghcr-service').use(auth({ username: 'buzz' })).send({ tag: 'foo' });
+        });
+
+        after(function () {
+          execStub.restore();
+          delete this.res;
+        });
+
+        it('returns a status 404', async function () {
+          expect(this.res.status).to.equal(404);
+        });
+
+        it('returns a meaningful error message', async function () {
+          expect(this.res.text).to.equal('ghcr.io/nasa/my-repository:foo is unreachable');
+        });
+      });
+    });
+
+    describe('when the user is in the deployers group and a valid tag is sent in the request', async function () {
+      let execStub;
+      hookDescribeImage({
+        imageDigest: '',
+        lastUpdated: undefined,
+      });
+      before(async function () {
+        // resolve without error meaning script executed OK
+        execStub = sinon.stub(serviceImageTags, 'asyncExec').callsFake(() => Promise.resolve({}));
+        this.res = await request(this.frontend).put('/service-image-tag/harmony-service-example').use(auth({ username: 'buzz' })).send({ tag: 'foo' });
+
+      });
+
+      after(function () {
+        execStub.restore();
         delete this.res;
       });
 
@@ -345,12 +555,19 @@ describe('Service image endpoint', async function () {
     });
 
     describe('when the user is in the admin group and a valid tag is sent in the request', async function () {
+      let execStub;
+      hookDescribeImage({
+        imageDigest: '',
+        lastUpdated: undefined,
+      });
       before(async function () {
-        hookRedirect('adam');
+        // resolve to zero exit code meaning script executed OK
+        execStub = sinon.stub(serviceImageTags, 'asyncExec').callsFake(() => Promise.resolve(0));
         this.res = await request(this.frontend).put('/service-image-tag/harmony-service-example').use(auth({ username: 'adam' })).send({ tag: 'foo' });
       });
 
       after(function () {
+        execStub.restore();
         delete this.res;
       });
 
