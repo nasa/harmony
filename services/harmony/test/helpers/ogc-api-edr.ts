80b7fb7ee2435966139235fa6b4ff436fdc1f061
Yonggang Liu <ygliuvt@gmail.com>
2024-05-03 14:46:44 -0400

    HARMONY-1736: Minor cleanup.

--- services/harmony/test/helpers/ogc-api-edr.ts
+++ services/harmony/test/helpers/ogc-api-edr.ts
@@ -1,9 +1,7 @@
-import { expect } from 'chai';
 import { parse } from 'cookie';
 import { Application } from 'express';
-import { after, before, describe, it } from 'mocha';
+import { after, before } from 'mocha';
 import request, { Test } from 'supertest';
-import * as url from 'url';
 import { auth } from './auth';
 
 export const defaultCollection = 'C1233800302-EEDTEST';
@@ -51,7 +49,7 @@ function cookieValue(encodedValue: string, key: string): string {
 }
 
 /**
- * Adds before/after hooks to navigate from the coverages landing page to a related resource
+ * Adds before/after hooks to navigate from the EDR landing page to a related resource
  *
  * @param collection - The CMR Collection ID to query
  * @param version - The OGC API - EDR version to use
@@ -70,9 +68,8 @@ export function hookLandingPage(collection: string, version: string): void {
  * Performs getDataForArea request on the given collection with the given params
  *
  * @param app - The express application (typically this.frontend)
- * @param version - The OGC coverages API version
+ * @param version - The OGC EDR API version
  * @param collection - The CMR Collection ID to perform a service on
- * @param coverageId - The coverage ID(s) / variable name(s), or "all"
  * @param options - additional options for the request
  * @returns The response
  */
@@ -84,8 +81,6 @@ export function edrRequest(
     headers = {},
     cookies = null }: QueryOptions = {},
 ): Test {
-  // const encodedCoverageId = encodeURIComponent(coverageId);
-  // query.variable = encodedCoverageId;
   let req = request(app)
     .get(`/ogc-api-edr/${version}/collections/${collection}/area`)
     .query(query)
@@ -104,7 +99,6 @@ export function edrRequest(
  * @param app - The express application (typically this.frontend)
  * @param version - The OGC version
  * @param collection - The CMR Collection ID to perform a service on
- * @param coverageId - The coverage ID(s) / variable name(s), or "all"
  * @param form - The form parameters to pass to the request
  * @param queryString - The query string parameters to pass to the request
  * @returns An 'awaitable' object that resolves to a Response
@@ -113,7 +107,6 @@ export function postEdrRequest(
   app: Express.Application, version: string, collection: string, form: object, queryString = '',
 ): Test {
   let urlPathAndParam = `/ogc-api-edr/${version}/collections/${collection}/area`;
-  console.log(`==============form: ${JSON.stringify(form)}`);
   if (queryString) urlPathAndParam += `?${queryString}`;
   let req = request(app)
     .post(urlPathAndParam);
@@ -134,9 +127,8 @@ export function postEdrRequest(
 /**
  * Adds before/after hooks to run an OGC API getDataForArea request
  *
- * @param version - The OGC coverages API version
+ * @param version - The OGC EDR API version
  * @param collection - The CMR Collection ID to perform a service on
- * @param coverageId - The coverage ID(s) / variable name(s), or "all"
  * @param options - additional options for the request
  */
 export function hookEdrRequest(
@@ -168,37 +160,10 @@ export function hookEdrRequest(
 }
 
 /**
- * Adds before/after hooks to run an OGC API getDataForArea request synchronously
- * by adding a default granule ID to the request.  Caller must ensure that the resulting
- * request is not constrained such that the granule is excluded
- *
- * @param version - The OGC coverages API version
- * @param collection - The CMR Collection ID to perform a service on
- * @param coverageId - The coverage ID(s) / variable name(s), or "all"
- * @param options - additional options for the request
- */
-export function hookSyncEdrRequest(
-  version?: string,
-  collection?: string,
-  {
-    query = {},
-    headers = {},
-    username = 'anonymous',
-  }: QueryOptions = {},
-): void {
-  hookEdrRequest(
-    version,
-    collection,
-    { query: { granuleId: defaultGranuleId, ...query }, headers, username },
-  );
-}
-
-/**
  * Adds before/after hooks to run a POST getDataForArea request
  *
  * @param version - The OGC API version
  * @param collection - The CMR Collection ID to perform a service on
- * @param coverageId - The coverage ID(s) / variable name(s), or "all"
  * @param form - The form data to be POST'd
  * @param queryString - The query string parameters to pass to the request
  */
@@ -247,178 +212,6 @@ export function hookPostEdrRequest(
       }
     }
   });
-  after(function () {
-    delete this.res;
-  });
-}
-
-/**
- * Adds before/after hooks to run an OGC API getDataForArea default request
- * where the caller just needs to run a request without caring about any of the
- * parameters to send to the request.
- *
- */
-export function hookDefaultEdrRequest(): void {
-  hookEdrRequest(defaultVersion,
-    defaultCollection,
-    {
-      query: { variable: defaultCoverageId },
-      username: 'anonymous',
-    });
-}
-
-/**
- * Asserts that a link relation exists, then loads it, allowing the passed function to provide
- * further specs about its contents.  Expects the current page response to exist in the `this.res`
- * object.
- *
- * @param rel - The link relation to find in the "links" array of the current response
- * @param description - A human-readable name for what the relation is, e.g.
- *   "the Open API Spec"
- * @param fn - The body of the describe statement
- */
-export function describeRelation(rel: string, description: string, fn: Function): void {
-  it(`provides a link relation, \`${rel}\`, to ${description}`, function () {
-    const parsedBody = JSON.parse(this.res.text);
-    expect(parsedBody).to.have.key('links');
-    const link = parsedBody.links.find((l) => l.rel === rel);
-    expect(link, `${parsedBody.links} did not include relation ${rel}`).to.be.ok;
-  });
-
-  describe(`when following the \`${rel}\` relation to ${description}`, async function () {
-    before(async function () {
-      // Grab the link with the correct relation
-      const parsedBody = JSON.parse(this.res.text);
-      const link = parsedBody.links.find((l) => l.rel === rel);
-
-      // Push the current response onto a stack so we can get back to it when we're done
-      this.resStack = this.resStack || [];
-      this.resStack.push(this.res);
-
-      // Request the links href as a path relative to the app root (required by supertest)
-      // and save that into the current `this.res` field
-      const parsedLink = new url.URL(link.href);
-      const relativeLink = [parsedLink.pathname, parsedLink.search].join('?');
-      this.res = await request(this.frontend).get(relativeLink);
-    });
-
-    after(function () {
-      // Restore `this.res`.  Not worried about cleaning up a possibly empty array.
-      this.res = this.resStack.pop();
-    });
-
-    await fn.bind(this)();
-  });
-}
-
-/**
- * Makes a call to return the OGC API EDR Open API spec.
- *
- * @param app - The express application (typically this.frontend)
- * @param collection - The CMR Collection ID to query
- * @param version - The specification version
- * @returns The response
- */
-export function coveragesSpecRequest(
-  app: Express.Application, collection: string, version: string,
-): request.Test {
-  return request(app).get(`/${collection}/ogc-api-coverages/${version}/api`);
-}
-
-/**
- * Makes a call to return the OGC API EDR landing page.
- *
- * @param app - The express application (typically this.frontend)
- * @param collection - The CMR Collection ID to query
- * @param version - The specification version
- * @returns The response
- */
-export function coveragesLandingPageRequest(
-  app: Express.Application, collection: string, version: string,
-): request.Test {
-  return request(app).get(`/${collection}/ogc-api-coverages/${version}/`);
-}
-
-/**
- * Makes a call to return the OGC API EDR describe collections page.
- *
- * @param app - The express application (typically this.frontend)
- * @param collection - The CMR Collection ID to query
- * @param version - The specification version
- * @param query - The query parameters to pass to the describe collections request
- * @returns The response
- */
-export function describeCollectionsRequest(
-  app: Express.Application, collection: string, version: string, query: object,
-): request.Test {
-  return request(app)
-    .get(`/${collection}/ogc-api-coverages/${version}/collections`)
-    .query(query);
-}
-
-/**
- * Adds before/after hooks when calling the OGC API EDR describe collections page.
- *
- * @param collection - The CMR Collection ID to query
- * @param version - The specification version
- * @param query - The query parameters to pass to the describe collections request
- */
-export function hookDescribeCollectionsRequest(
-  collection: string, version: string, query: object = {},
-): void {
-  before(async function () {
-    this.res = await describeCollectionsRequest(this.frontend, collection, version, query);
-  });
-
-  after(function () {
-    delete this.res;
-  });
-}
-
-/**
- * Makes a call to return the OGC API EDR describe collections page.
- *
- * @param app - The express application (typically this.frontend)
- * @param collection - The CMR Collection ID to query
- * @param version - The specification version
- * @param variablePath - The full path of the variable
- * @param query - The query parameters to pass to the describe collections request
- * @returns The response
- */
-export function describeCollectionRequest(
-  app: Express.Application,
-  collection: string,
-  version: string,
-  variablePath: string,
-  query: object,
-): request.Test {
-  const encodedPath = encodeURIComponent(variablePath);
-  return request(app)
-    .get(`/${collection}/ogc-api-coverages/${version}/collections/${encodedPath}`)
-    .query(query);
-}
-
-/**
- * Adds before/after hooks when calling the OGC API EDR describe collections page.
- *
- * @param collection - The CMR Collection ID to query
- * @param version - The specification version
- * @param variablePath - The full path of the variable
- * @param query - The query parameters to pass to the describe collections request
- */
-export function hookDescribeCollectionRequest(
-  collection: string, version: string, variablePath: string, query: object = {},
-): void {
-  before(async function () {
-    this.res = await describeCollectionRequest(
-      this.frontend,
-      collection,
-      version,
-      variablePath,
-      query,
-    );
-  });
-
   after(function () {
     delete this.res;
   });
