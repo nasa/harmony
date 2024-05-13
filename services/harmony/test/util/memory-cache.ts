075d0cccf9e9cfd9ab2ced8f000abd08200db261
James Norton <james.norton@nasa.gov>
2024-05-13 10:52:50 -0400

    HARMONY-1764: Add tests for memory-cache

--- services/harmony/test/util/memory-cache.ts
+++ services/harmony/test/util/memory-cache.ts
@@ -9,15 +9,61 @@ describe('MemoryCache', function () {
     async function fakeFetchMethod(_key: string): Promise<string> {
       return 'bar';
     }
+
     let fetchMethodSpy;
+    let cache;
+
     beforeEach(function () {
       fetchMethodSpy = spy(fakeFetchMethod);
+      cache = new MemoryCache(fetchMethodSpy);
     });
 
-    it('calls fetchMethod for missing keys', async function () {
-      const cache = new MemoryCache(fetchMethodSpy);
+    it('calls the fetcher callback for missing keys', async function () {
       await cache.fetch('foo');
-      expect(fetchMethodSpy.called);
+      expect(fetchMethodSpy.called).to.equal(true);
+    });
+
+    it('does not call the fetcher callback for set keys', async function () {
+      await cache.set('foo', 'bar');
+      await cache.fetch('foo');
+      expect(fetchMethodSpy.called).to.equal(false);
+    });
+
+    it('does not call the fetcher callback more than once for a key', async function () {
+      await cache.fetch('foo');
+      await cache.fetch('foo');
+      expect(fetchMethodSpy.calledOnce).to.equal(true);
+    });
+
+    it('returns the proper value for a given key', async function () {
+      const value = await cache.fetch('foo');
+      expect(value).to.equal('bar');
+    });
+  });
+
+  describe('set and get', async function () {
+    let fetchMethodSpy;
+    let cache;
+
+    beforeEach(function () {
+      fetchMethodSpy = spy();
+      cache = new MemoryCache(fetchMethodSpy);
+    });
+
+    it('returns `undefined` for missing keys', async function () {
+      const value = await cache.get('foo');
+      expect(value).to.equal(undefined);
+    });
+
+    it('does not call the fetcher callback function for missing keys', async function () {
+      await cache.get('foo');
+      expect(fetchMethodSpy.called).to.equal(false);
+    });
+
+    it('returns the proper value for a set key', async function () {
+      await cache.set('foo', 'baz');
+      const value = await cache.get('foo');
+      expect(value).to.equal('baz');
     });
   });
 });
