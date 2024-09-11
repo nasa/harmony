ead37c6a0bf2f2c45294602a4985174154ba1894
James Norton <james.norton@nasa.gov>
2024-09-11 13:03:13 -0400

    HARMONY-1876: Change pg image to include pg-vecor and add migration to enable it

---
+++ db/migrations/20240911164432_enable_pg_vector.js
@@ -0,0 +1,15 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.raw('CREATE EXTENSION IF NOT EXISTS vector');
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.raw('DROP EXTENSION IF EXISTS vector');
+};
