751a258f8d7e98232d3261270b390126df67b99e
vinny <vinny.inverso@gmail.com>
2024-10-09 13:22:46 -0400

    HARMONY-1892: Show all job checkboxes on non-admin workflow ui to enable labeling

--- services/harmony/app/frontends/workflow-ui.ts
+++ services/harmony/app/frontends/workflow-ui.ts
@@ -159,10 +159,11 @@ function getPaginationDisplay(pagination: ILengthAwarePagination): { from: strin
  * a row of the jobs table.
  * @param logger - the logger to use
  * @param requestQuery - the query parameters from the request
- * @param checked - whether the job should be selected
+ * @param isAdminRoute - whether the current endpoint being accessed is /admin/*
+ * @param jobIDs - list of IDs for selected jobs
  * @returns an object with rendering functions
  */
-function jobRenderingFunctions(logger: Logger, requestQuery: Record<string, any>, jobIDs: string[] = []): object {
+function jobRenderingFunctions(logger: Logger, requestQuery: Record<string, any>, isAdminRoute: boolean, jobIDs: string[] = []): object {
   return {
     jobBadge(): string {
       return statusClass[this.status];
@@ -205,7 +206,7 @@ function jobRenderingFunctions(logger: Logger, requestQuery: Record<string, any>
     },
     jobSelectBox(): string {
       const checked = jobIDs.indexOf(this.jobID) > -1 ? 'checked' : '';
-      if (this.hasTerminalStatus()) {
+      if (this.hasTerminalStatus() && isAdminRoute) {
         return '';
       }
       return `<input id="select-${this.jobID}" class="select-job" type="checkbox" data-id="${this.jobID}" data-status="${this.status}" autocomplete="off" ${checked}></input>`;
@@ -323,7 +324,7 @@ export async function getJobs(
     const previousPage = pageLinks.find((l) => l.rel === 'prev');
     const currentPage = pageLinks.find((l) => l.rel === 'self');
     const paginationDisplay = getPaginationDisplay(pagination);
-    const selectAllBox = jobs.some((j) => !j.hasTerminalStatus()) ?
+    const selectAllBox = !isAdminRoute || jobs.some((j) => !j.hasTerminalStatus()) ?
       '<input id="select-jobs" type="checkbox" title="select/deselect all jobs" autocomplete="off">' : '';
     res.render('workflow-ui/jobs/index', {
       version,
@@ -355,7 +356,7 @@ export async function getJobs(
         { ...nextPage, linkTitle: 'next' },
         { ...lastPage, linkTitle: 'last' },
       ],
-      ...jobRenderingFunctions(req.context.logger, requestQuery),
+      ...jobRenderingFunctions(req.context.logger, requestQuery, isAdminRoute),
     });
   } catch (e) {
     req.context.logger.error(e);
@@ -626,24 +627,25 @@ export async function getJobsTable(
   req: HarmonyRequest, res: Response, next: NextFunction,
 ): Promise<void> {
   try {
+    const isAdminRoute = req.context.isAdminAccess;
     const { jobIDs } = req.body;
     const { isAdmin } = await getEdlGroupInformation(
       req.user, req.context.logger,
     );
     const requestQuery = keysToLowerCase(req.query);
-    const { tableQuery } = parseQuery(requestQuery, JobStatus, req.context.isAdminAccess);
+    const { tableQuery } = parseQuery(requestQuery, JobStatus, isAdminRoute);
     const jobQuery = tableQueryToJobQuery(tableQuery, isAdmin, req.user);
     const { page, limit } = getPagingParams(req, env.defaultJobListPageSize, 1, true, true);
     const jobsRes = await Job.queryAll(db, jobQuery, page, limit);
     const jobs = jobsRes.data;
     const { pagination } = jobsRes;
-    const selectAllChecked = jobs.every((j) => j.hasTerminalStatus() || (jobIDs.indexOf(j.jobID) > -1)) ? 'checked' : '';
-    const selectAllBox = jobs.some((j) => !j.hasTerminalStatus()) ?
+    const selectAllChecked = jobs.every((j) => (j.hasTerminalStatus() && isAdminRoute) || (jobIDs.indexOf(j.jobID) > -1)) ? 'checked' : '';
+    const selectAllBox = !isAdminRoute || jobs.some((j) => !j.hasTerminalStatus()) ?
       `<input id="select-jobs" type="checkbox" title="select/deselect all jobs" autocomplete="off" ${selectAllChecked}>` : '';
     const tableContext = {
       jobs,
       selectAllBox,
-      ...jobRenderingFunctions(req.context.logger, requestQuery, jobIDs),
+      ...jobRenderingFunctions(req.context.logger, requestQuery, isAdminRoute, jobIDs),
       isAdminRoute: req.context.isAdminAccess,
     };
     const tableHtml = await new Promise<string>((resolve, reject) => req.app.render(
