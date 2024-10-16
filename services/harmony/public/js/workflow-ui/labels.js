b901370f47f4ac017a5671f66a77134a8769d51b
vinny <vinny.inverso@gmail.com>
2024-10-16 15:56:06 -0400

    HARMONY-1892: Simple label filter/search

--- services/harmony/public/js/workflow-ui/labels.js
+++ services/harmony/public/js/workflow-ui/labels.js
@@ -7,10 +7,27 @@ function handleLabelClick(event) {
   labelElement.classList.toggle('active');
 }
 
+const labelItems = document.querySelectorAll('#labels-list .label-li');
+
+/**
+ *
+ */
+function filterList() {
+  const searchValue = document.querySelector('#label-search').value.toLowerCase().trim();
+  for (const labelItem of labelItems) {
+    const labelName = labelItem.innerText.toLowerCase().trim();
+    labelItem.style.display = labelName.startsWith(searchValue) ? '' : 'none';
+  }
+}
+
 /**
  *
  */
 function bindEventListeners() {
+  const labelSearchElement = document.getElementById('label-search');
+  labelSearchElement.addEventListener('keyup', () => {
+    filterList();
+  });
   document.querySelectorAll('.label-item').forEach((item) => {
     item.addEventListener('click', (event) => {
       handleLabelClick(event);
