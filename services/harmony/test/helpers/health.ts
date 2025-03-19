bfba48d30d76775aa20eb605059ae00a70a6e909
Yonggang Liu <ygliuvt@gmail.com>
2025-03-19 09:34:48 -0400

    HARMONY-1986: Add CMR and EDL dependencies to health check.

--- services/harmony/test/helpers/health.ts
+++ services/harmony/test/helpers/health.ts
@@ -1,5 +1,26 @@
+import sinon from 'sinon';
 import request from 'supertest';
 import { hookRequest } from './hooks';
+import * as cmr from '../../app/util/cmr';
+import * as edl from '../../app/util/edl-api';
+
+/**
+ * Hooks to stub CMR and EDL health checks for specific tests.
+ */
+export function hookCmrEdlHealthCheck(cmrStatus: object, edlStatus: boolean): void {
+  let cmrStub;
+  let edlStub;
+
+  before(function () {
+    cmrStub = sinon.stub(cmr, 'getCmrHealth').callsFake(async () => cmrStatus);
+    edlStub = sinon.stub(edl, 'isEdlHealthy').callsFake(async () => edlStatus);
+  });
+
+  after(function () {
+    cmrStub.restore();
+    edlStub.restore();
+  });
+}
 
 /**
  * Makes a /admin/health request
