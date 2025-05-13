6bb416b28ea267845f1334b78914eb63b513e1cc
Chris Durbin <christopher.d.durbin@nasa.gov>
2025-05-13 11:37:21 -0400

    HARMONY-2066: Address test expectations to expect content-type header.

--- services/harmony/test/middleware/external-validation.ts
+++ services/harmony/test/middleware/external-validation.ts
@@ -122,7 +122,10 @@ describe('external validation', function () {
       it('sets the Authorization header on the request to the external validator', async function () {
         const nextStub = stub();
         await externalValidation(this.reqWithValidation, this.res, nextStub);
-        expect(options.headers).to.eql({ 'Authorization': `Bearer ${this.reqWithValidation.accessToken}` });
+        expect(options.headers).to.eql({
+          'Authorization': `Bearer ${this.reqWithValidation.accessToken}`,
+          'Content-type': 'application/json',
+        });
       });
 
     });
