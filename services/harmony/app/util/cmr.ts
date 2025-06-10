862eef53ec0df93c2db71cfbb17371a6562d2e42
James Norton <james.norton@nasa.gov>
2025-06-10 12:49:00 -0400

    HARMONY-2069: Remove inadvertent insertion

--- services/harmony/app/util/cmr.ts
+++ services/harmony/app/util/cmr.ts
@@ -76,7 +76,6 @@ export interface CmrCollection {
     visualizations?: string[];
   };
   variables?: CmrUmmVariable[];
-  variable
   visualizations?: CmrUmmVisualization[];
   tags?: CmrTags;
   eula_identifiers?: string[];
