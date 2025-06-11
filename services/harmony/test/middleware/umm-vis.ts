d90f41a0322484938e12bb8e18ef691dc6042e19
James Norton <james.norton@nasa.gov>
2025-06-11 16:06:00 -0400

    HARMONY-2069: Fix tests for UMM-Vis

--- services/harmony/test/middleware/umm-vis.ts
+++ services/harmony/test/middleware/umm-vis.ts
@@ -1,10 +1,9 @@
 import { expect } from 'chai';
 
 import DataOperation from '../../app/models/data-operation';
-import { getWorkflowStepsByJobId } from '../../app/models/workflow-steps';
-import db from '../../app/util/db';
 import { hookRangesetRequest } from '../helpers/ogc-api-coverages';
 import hookServersStartStop from '../helpers/servers';
+import StubService from '../helpers/stub-service';
 
 const collectionId = 'C1273843214-EEDTEST';
 const validVariable = 'blue_var';
@@ -12,50 +11,48 @@ const validVariable = 'blue_var';
 describe('UMM-Vis', function () {
   hookServersStartStop();
   describe('When `all` is given as the variable', function () {
-    hookRangesetRequest('1.0.0', collectionId, 'all', { query: {}, username: 'joe' });
+    StubService.hook({ params: { redirect: 'http://example.com' } });
+    hookRangesetRequest('1.0.0', collectionId, 'all', { query: {} });
 
-    it('the data operation contains the collletions visualization records at the top-level in the source', async function () {
-      const job = JSON.parse(this.res.text);
-      console.log(`JOB: ${JSON.stringify(job)}`);
-      const workflowSteps = await getWorkflowStepsByJobId(db, job.jobID);
-      const operation = JSON.parse(workflowSteps[0].operation) as DataOperation;
+    it('the data operation contains the collletions visualization records at the top-level in the source', function () {
+      const operation = this.service.operation as DataOperation;
       expect(operation.sources[0].visualizations.length).equals(2);
+      expect((operation.sources[0].visualizations[0] as { Name: string; }).Name).equal('Harmony_umm_vis_test_1');
+      expect((operation.sources[0].visualizations[1] as { Name: string; }).Name).equal('Harmony_umm_vis_test_3');
     });
 
   });
   describe('When a variable is specified in the url', function () {
-    hookRangesetRequest('1.0.0', collectionId, validVariable, { query: {}, username: 'joe' });
-    describe('and a service requests work', function () {
-      it('the data operation contains the variable visaulization records underneath the variable in the source', async function () {
-        const job = JSON.parse(this.res.text);
-        const workflowSteps = await getWorkflowStepsByJobId(db, job.jobID);
-        const operation = JSON.parse(workflowSteps[0].operation) as DataOperation;
-        expect(operation.sources[0].variables[0].visualizations.length).equals(2);
-      });
-      it('the data operation does not include the collection visualizations', async function () {
-        const job = JSON.parse(this.res.text);
-        const workflowSteps = await getWorkflowStepsByJobId(db, job.jobID);
-        const operation = JSON.parse(workflowSteps[0].operation) as DataOperation;
-        expect(operation.sources[0].visualizations.length).equals(0);
-      });
+    StubService.hook({ params: { redirect: 'http://example.com' } });
+    hookRangesetRequest('1.0.0', collectionId, validVariable, { query: {} });
+
+    it('the data operation contains the variable visaulization records underneath the variable in the source', async function () {
+      const operation = this.service.operation as DataOperation;
+      expect(operation.sources[0].variables[0].visualizations.length).equals(2);
+      expect((operation.sources[0].variables[0].visualizations[0] as { Name: string; }).Name).equal('Harmony_umm_vis_test_2');
+      expect((operation.sources[0].variables[0].visualizations[1] as { Name: string; }).Name).equal('Harmony_umm_vis_test_3');
+
+    });
+    it('the data operation does not include the collection visualizations', async function () {
+      const operation = this.service.operation as DataOperation;
+      expect(operation.sources[0].visualizations).to.be.undefined;
     });
   });
 
-  describe('When a variable is specified in the query parameters', function () {
-    hookRangesetRequest('1.0.0', collectionId, 'query_parameters', { query: { variable: validVariable }, username: 'joe' });
-    describe('and a service requests work', function () {
-      it('the data operation contains the variable visaulization records underneath the variable in the source', async function () {
-        const job = JSON.parse(this.res.text);
-        const workflowSteps = await getWorkflowStepsByJobId(db, job.jobID);
-        const operation = JSON.parse(workflowSteps[0].operation) as DataOperation;
-        expect(operation.sources[0].variables[0].visualizations.length).equals(2);
-      });
-      it('the data operation does not include the collection visualizations', async function () {
-        const job = JSON.parse(this.res.text);
-        const workflowSteps = await getWorkflowStepsByJobId(db, job.jobID);
-        const operation = JSON.parse(workflowSteps[0].operation) as DataOperation;
-        expect(operation.sources[0].visualizations.length).equals(0);
-      });
+  describe('When a variable is specified in the url', function () {
+    StubService.hook({ params: { redirect: 'http://example.com' } });
+    hookRangesetRequest('1.0.0', collectionId, 'parameter_vars', { query: { variable: validVariable } });
+
+    it('the data operation contains the variable visaulization records underneath the variable in the source', async function () {
+      const operation = this.service.operation as DataOperation;
+      expect(operation.sources[0].variables[0].visualizations.length).equals(2);
+      expect((operation.sources[0].variables[0].visualizations[0] as { Name: string; }).Name).equal('Harmony_umm_vis_test_2');
+      expect((operation.sources[0].variables[0].visualizations[1] as { Name: string; }).Name).equal('Harmony_umm_vis_test_3');
+
+    });
+    it('the data operation does not include the collection visualizations', async function () {
+      const operation = this.service.operation as DataOperation;
+      expect(operation.sources[0].visualizations).to.be.undefined;
     });
   });
 });
