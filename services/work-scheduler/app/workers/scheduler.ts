7f7e3edc4da05716770cb6cdf123825b648ede9d
Chris Durbin <christopher.d.durbin@nasa.gov>
2025-10-09 10:41:56 -0400

    HARMONY-2189: Speed up work scheduler processing. Cache some calls, delete messages in batches, queue up much larger number of work items when service queue appears to be starved.

--- services/work-scheduler/app/workers/scheduler.ts
+++ services/work-scheduler/app/workers/scheduler.ts
@@ -1,21 +1,75 @@
+import _ from 'lodash';
+import { LRUCache } from 'lru-cache';
+import { Logger } from 'winston';
+
 import * as k8s from '@kubernetes/client-node';
-import { Worker } from '../../../harmony/app/workers/worker';
-import env from '../util/env';
+
+import { QUERY_CMR_SERVICE_REGEX } from '../../../harmony/app/backends/workflow-orchestration/util';
+import {
+  getWorkItemsFromDatabase,
+} from '../../../harmony/app/backends/workflow-orchestration/work-item-polling';
 import logger from '../../../harmony/app/util/log';
 import { logAsyncExecutionTime } from '../../../harmony/app/util/log-execution';
-import { Logger } from 'winston';
-import { getQueueUrlForService, getQueueForUrl, getWorkSchedulerQueue, getQueueForType } from '../../../harmony/app/util/queue/queue-factory';
-import { QUERY_CMR_SERVICE_REGEX } from '../../../harmony/app/backends/workflow-orchestration/util';
-import { getWorkItemsFromDatabase } from '../../../harmony/app/backends/workflow-orchestration/work-item-polling';
-import { getPodsCountForPodName, getPodsCountForService } from '../util/k8s';
 import { Queue, ReceivedMessage, WorkItemQueueType } from '../../../harmony/app/util/queue/queue';
+import {
+  getQueueForType, getQueueForUrl, getQueueUrlForService, getWorkSchedulerQueue,
+} from '../../../harmony/app/util/queue/queue-factory';
 import sleep from '../../../harmony/app/util/sleep';
+import { Worker } from '../../../harmony/app/workers/worker';
+import env from '../util/env';
+import { getPodsCountForPodName, getPodsCountForService } from '../util/k8s';
 
 const kc = new k8s.KubeConfig();
 kc.loadFromDefault();
 
 export const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
 const SCHEDULER_POD_NAME = 'harmony-work-scheduler';
+
+/**
+ * Calls the k8s API to get a count of the number of pods with the given serviceID
+ * @param serviceID - the service id
+ * @param _sv - The "stale value" from LRUCache; unused in this implementation
+ * @param reqLogger - the logger for the request
+ * @returns the pod count
+ */
+async function servicePodCountFetcher(serviceID: string, _sv, { context: fetchContext }): Promise<number> {
+  const timed = await logAsyncExecutionTime(
+    getPodsCountForService,
+    'PSQ.getPodsCountForService',
+    fetchContext.logger,
+  );
+  return timed(serviceID);
+}
+
+/**
+ * Calls the k8s API to get a count of the number of pods with the given podName
+ * @param podName - the pod name
+ * @param _sv - The "stale value" from LRUCache; unused in this implementation
+ * @param reqLogger - the logger for the request
+ * @returns the pod count
+ */
+async function schedulerPodCountFetcher(podName: string, _sv, { context: fetchContext }): Promise<number> {
+  const timed = await logAsyncExecutionTime(
+    getPodsCountForPodName,
+    'PSQ.getSchedulerPodsCount',
+    fetchContext.logger,
+  );
+  return timed(podName);
+}
+
+const TWO_MINUTES_MS = 2 * 60 * 1000;
+
+const servicePodCountCache = new LRUCache<string, number>({
+  ttl: TWO_MINUTES_MS,
+  max: 1000,
+  fetchMethod: servicePodCountFetcher,
+});
+
+const schedulerPodCountCache = new LRUCache<string, number>({
+  ttl: TWO_MINUTES_MS,
+  max: 1000,
+  fetchMethod: schedulerPodCountFetcher,
+});
 
 /**
  * Read all the messages from a queue (up to the default timeout period) and return them.
@@ -81,11 +135,27 @@ function sizeToBatches(
  * @param schedulerPodCount - number of work scheduler pods running
  * @param queuedCount - current number of messages on the service queue
  * @param scaleFactor - percent of the number of messages we want
+ * @param numMessagesReceived - the number of messages the work scheduler received for this service
  * @returns the number of work items to request to queue
  */
 export function calculateNumItemsToQueue(
   servicePodCount: number, schedulerPodCount: number, queuedCount: number, scaleFactor: number,
+  numMessagesReceived: number,
 ): number {
+  // If we received a large number of messages that means each pod is starved for work and there's
+  // nothing it was able to pick up on the queue. In that scenario we should queue a large number
+  // of messages
+  // If there are hardly any messages on the queue we want to try to figure out if there are
+  // many pods asking for work meaning we've starved the queue. If there are then we want to queue
+  // a large number of messages.
+  if (queuedCount <= 0.1 * servicePodCount) {
+    const fullQueueCount = servicePodCount - queuedCount;
+
+    // Queue enough work to match the number of messages received without queueing more than the
+    // number of available service pods. Always return at least 1 item to be queued.
+    return Math.max(1, Math.min(fullQueueCount, numMessagesReceived));
+  }
+
   const minOneSchedulerPodCount = Math.max(1, schedulerPodCount);
   const numItemsToQueue = scaleFactor * (servicePodCount / minOneSchedulerPodCount) - queuedCount;
   let numItemsToQueueInt = Math.max(0, Math.floor(numItemsToQueue));
@@ -97,6 +167,11 @@ export function calculateNumItemsToQueue(
   }
   return numItemsToQueueInt;
 }
+
+type QueueItem = {
+  body: string;        // serviceID
+  receipt: string;     // SQS receiptHandle
+};
 
 /**
  * Read the scheduler queue and process any items in it
@@ -120,8 +195,6 @@ export async function processSchedulerQueue(
     'PSQ.drainQueue',
     reqLogger))(schedulerQueue, reqLogger);
 
-  const processedServiceIDs: string[] = [];
-
   reqLogger.debug(`Found ${queueItems.length} items in the scheduler queue`);
 
   if (env.maxWorkItemsOnUpdateQueue !== -1) {
@@ -133,80 +206,76 @@ export async function processSchedulerQueue(
       return;
     }
   }
-  for (const queueItem of queueItems) {
-    const serviceID = queueItem.body;
-    if (!processedServiceIDs.includes(serviceID)) {
-      processedServiceIDs.push(serviceID);
-      reqLogger.info(`Processing scheduler queue item for service ${serviceID}`);
-      const queueUrl = getQueueUrlForService(serviceID);
-      const queue = getQueueForUrl(queueUrl);
-      if (!queue) {
-        throw new Error(`No queue found for URL ${queueUrl}`);
-      }
 
-      // Get the number of messages in the queue and the number of pods for the service
-      // so we can determine how many work items to send
-      const messageCountStart = new Date();
-      const mcStartTime = new Date().getTime();
-      const messageCount = await queue.getApproximateNumberOfMessages();
-      durationMs = new Date().getTime() - mcStartTime;
-      logger.debug('timing.PSQ.queue.getApproximateNumberOfMessages.end', { durationMs });
-      const messageCountEnd = new Date();
-      const servicePodCount = await (await logAsyncExecutionTime(
-        getPodsCountForService,
-        'PSQ.getPodsCountForService',
-        reqLogger))(serviceID);
+  const groupedByServiceID: Record<string, QueueItem[]> = _.groupBy(queueItems, item => item.body);
 
-      const schedulerPodCount = await (await logAsyncExecutionTime(
-        getPodsCountForPodName,
-        'PSQ.getSchedulerPodsCount',
-        reqLogger))(SCHEDULER_POD_NAME);
-
-      const podCountEnd = new Date();
-      const messageCountTime = messageCountEnd.getTime() - messageCountStart.getTime();
-      const podCountTime = podCountEnd.getTime() - messageCountEnd.getTime();
-      reqLogger.debug(`Message count took ${messageCountTime}ms`, { durationMs: messageCountTime });
-      reqLogger.debug(`Pod count took ${podCountTime}ms`, { durationMs: podCountTime });
-
-      let scaleFactor = env.serviceQueueBatchSizeCoefficient;
-      // Use a different scale factor for fast services (now only query-cmr)
-      if (QUERY_CMR_SERVICE_REGEX.test(serviceID)) {
-        scaleFactor = env.fastServiceQueueBatchSizeCoefficient;
-      }
-
-      const workSize = calculateNumItemsToQueue(servicePodCount, schedulerPodCount, messageCount, scaleFactor);
-      reqLogger.debug(`Work size count is ${workSize} based on service pod count of ${servicePodCount}, message count ${messageCount}, scheduler pod count ${schedulerPodCount} and scaleFactor ${scaleFactor} for queue ${queueUrl}`);
-      reqLogger.debug(`Attempting to retrieve ${workSize} work items for queue ${queueUrl}`);
-
-      let queuedCount = 0;
-      const batchStartTime = new Date().getTime();
-      for (const chunk of sizeToBatches(workSize, env.workItemSchedulerBatchSize)) {
-        const workItems = await (await logAsyncExecutionTime(
-          getWorkItemsFromDatabase,
-          'PSQ.getWorkItemsFromDatabase',
-          reqLogger))(serviceID, reqLogger, chunk);
-
-        for (const workItem of workItems) {
-          const json = JSON.stringify(workItem);
-          reqLogger.info(`Sending work item ${workItem.workItem.id} to queue ${queueUrl}`);
-          const smStartTime = new Date().getTime();
-          await queue.sendMessage(json, `${workItem.workItem.id}`);
-          durationMs = new Date().getTime() - smStartTime;
-          logger.debug('timing.PSQ.queue.sendMessage.end', { durationMs });
-          queuedCount++;
-        }
-      }
-
-      durationMs = new Date().getTime() - batchStartTime;
-      logger.debug('timing.PSQ.batchProcessing.end', { durationMs });
-
-      reqLogger.info(`Sent ${queuedCount} work items to queue ${queueUrl}`);
+  for (const [serviceID, items] of Object.entries(groupedByServiceID)) {
+    const numMessagesReceived = items.length;
+    reqLogger.info(`Processing ${numMessagesReceived} queued messages for service ${serviceID}`);
+    reqLogger.info(`Processing scheduler queue item for service ${serviceID}`);
+    const queueUrl = getQueueUrlForService(serviceID);
+    const queue = getQueueForUrl(queueUrl);
+    if (!queue) {
+      throw new Error(`No queue found for URL ${queueUrl}`);
     }
+
+    // Get the number of messages in the queue and the number of pods for the service
+    // so we can determine how many work items to send
+    const messageCountStart = new Date();
+    const mcStartTime = new Date().getTime();
+    const messageCount = await queue.getApproximateNumberOfMessages();
+    durationMs = new Date().getTime() - mcStartTime;
+    logger.debug('timing.PSQ.queue.getApproximateNumberOfMessages.end', { durationMs });
+    const messageCountEnd = new Date();
+    const messageCountTime = messageCountEnd.getTime() - messageCountStart.getTime();
+    reqLogger.debug(`Message count took ${messageCountTime}ms`, { durationMs: messageCountTime });
+
+    // TODO - Cache both of these counts for up to 2 minutes
+    const servicePodCount = await servicePodCountCache.fetch(serviceID, { context: { logger: reqLogger } });
+    const schedulerPodCount = await schedulerPodCountCache.fetch(SCHEDULER_POD_NAME, { context: { logger: reqLogger } });
+    // const podCountEnd = new Date();
+
+    // const podCountTime = podCountEnd.getTime() - messageCountEnd.getTime();
+    // reqLogger.debug(`Pod count took ${podCountTime}ms`, { durationMs: podCountTime });
+
+    let scaleFactor = env.serviceQueueBatchSizeCoefficient;
+    // Use a different scale factor for fast services (now only query-cmr)
+    if (QUERY_CMR_SERVICE_REGEX.test(serviceID)) {
+      scaleFactor = env.fastServiceQueueBatchSizeCoefficient;
+    }
+
+    const workSize = calculateNumItemsToQueue(servicePodCount, schedulerPodCount, messageCount, scaleFactor, numMessagesReceived);
+    reqLogger.debug(`Work size count is ${workSize} based on service pod count of ${servicePodCount}, message count ${messageCount}, scheduler pod count ${schedulerPodCount}, numMessagesReceived ${numMessagesReceived}, and scaleFactor ${scaleFactor} for queue ${queueUrl}`);
+    reqLogger.debug(`Attempting to retrieve ${workSize} work items for queue ${queueUrl}`);
+
+    let queuedCount = 0;
+    const batchStartTime = new Date().getTime();
+    for (const chunk of sizeToBatches(workSize, env.workItemSchedulerBatchSize)) {
+      const workItems = await (await logAsyncExecutionTime(
+        getWorkItemsFromDatabase,
+        'PSQ.getWorkItemsFromDatabase',
+        reqLogger))(serviceID, reqLogger, chunk);
+
+      for (const workItem of workItems) {
+        const json = JSON.stringify(workItem);
+        reqLogger.info(`Sending work item ${workItem.workItem.id} to queue ${queueUrl}`);
+        const smStartTime = new Date().getTime();
+        await queue.sendMessage(json, `${workItem.workItem.id}`);
+        durationMs = new Date().getTime() - smStartTime;
+        logger.debug('timing.PSQ.queue.sendMessage.end', { durationMs });
+        queuedCount++;
+      }
+    }
+
+    durationMs = new Date().getTime() - batchStartTime;
+    logger.debug('timing.PSQ.batchProcessing.end', { durationMs });
+
+    reqLogger.info(`Sent ${queuedCount} work items to queue ${queueUrl}`);
 
     reqLogger.info('Sending delete message to scheduler queue');
 
     const dmStartTime = new Date().getTime();
-    await schedulerQueue.deleteMessage(queueItem.receipt);
+    await schedulerQueue.deleteMessages(items.map((item => item.receipt)));
     durationMs = new Date().getTime() - dmStartTime;
     logger.debug('timing.PSQ.queue.deleteMessage.end', { durationMs });
   }
