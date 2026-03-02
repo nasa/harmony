ef6024c3994a5f0cb1c1d88c8c24fa047a01fbbb
Josie Lyon <82548520+lyonthefrog@users.noreply.github.com>
2026-03-02 12:30:53 -0600

    HARMONY-2227: Allow Harmony to process the HOSS URL output format option that contains a semi-colon (#868)
    * HARMONY-2227: Add new hoss-opendap-url service chain to service configuration files.
    
    * HARMONY-2227: Handle output formats that contain quotes, spaces, or semi-colons.
    
    * HARMONY-2227: Update tests to test output format changes.
    
    * HARMONY-2227: Remove documentation in config files that says Harmony only matches before semi-colon.
    
    * HARMONY-2227: Updated hoss-opendap-url service record ID in services-prod.yml.
    
    * HARMONY-2227: Add unit test to show header content after semicolon is no longer stripped off.

--- services/harmony/test/util/content-negotiation.ts
+++ services/harmony/test/util/content-negotiation.ts
@@ -149,5 +149,14 @@ describe('util/content-negotiation', function () {
         expect(isMimeTypeAccepted('application/shapefile+zip', 'application/shapefile+zip')).to.be.true;
       });
     });
+
+    describe('when header contains a semicolon', function () {
+      it('returns true when there is an exact match', function () {
+        expect(isMimeTypeAccepted('application/x-netcdf4; profile=opendap_url', 'application/x-netcdf4; profile=opendap_url')).to.be.true;
+      });
+      it('returns false when content after semicolon is not stripped off', function () {
+        expect(isMimeTypeAccepted('application/x-netcdf4', 'application/x-netcdf4;profile=opendap_url')).to.be.false;
+      });
+    });
   });
 });
