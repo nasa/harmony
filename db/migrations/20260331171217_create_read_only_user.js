66653200aacd678050b9934c6f5652db14f01f3a
Matt Savoie <savoie@colorado.edu>
2026-04-06 12:20:16 -0600

    HARMONY-2318: Add read only access to the harmony database. (#888)

---
+++ db/migrations/20260331171217_create_read_only_user.js
@@ -0,0 +1,36 @@
+exports.up = async function(knex) {
+
+  const password = process.env.DATABASE_READONLY_PASSWORD;
+  if (!password) {
+    throw new Error('Unable to create harmony_read_only user: DATABASE_READONLY_PASSWORD variable is not set');
+  }
+
+  const { rows } = await knex.raw(
+    `SELECT 1 FROM pg_catalog.pg_roles WHERE rolname = 'harmony_read_only'`
+  );
+
+  if (rows.length === 0) {
+    const escapedPassword = password.replace(/'/g, "''");
+    await knex.raw(`CREATE ROLE harmony_read_only WITH LOGIN PASSWORD '${escapedPassword}'`);
+  }
+
+  await knex.raw(`GRANT USAGE ON SCHEMA public TO harmony_read_only`);
+  await knex.raw(`GRANT SELECT ON ALL TABLES IN SCHEMA public TO harmony_read_only`);
+  await knex.raw(`ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO harmony_read_only`);
+};
+
+exports.down = async function(knex) {
+
+  const { rows } = await knex.raw(
+    `SELECT 1 FROM pg_catalog.pg_roles WHERE rolname = 'harmony_read_only'`
+  );
+
+  if (rows.length === 0) {
+    return;
+  }
+
+  await knex.raw(`ALTER DEFAULT PRIVILEGES IN SCHEMA public REVOKE SELECT ON TABLES FROM harmony_read_only`);
+  await knex.raw(`REVOKE SELECT ON ALL TABLES IN SCHEMA public FROM harmony_read_only`);
+  await knex.raw(`REVOKE USAGE ON SCHEMA public FROM harmony_read_only`);
+  await knex.raw(`DROP ROLE IF EXISTS harmony_read_only`);
+};
