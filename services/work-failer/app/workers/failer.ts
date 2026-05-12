af1b264680bf895684f0e17ce0ad1c660840f840
Matt Savoie <matthew.savoie@colorado.edu>
2026-05-12 12:15:21 -0600

    HARMONY-2342: Adds net2cog timeout overrides for all ASF collections

--- services/work-failer/app/workers/failer.ts
+++ services/work-failer/app/workers/failer.ts
@@ -41,8 +41,12 @@ const serviceToDefaultTimeoutSeconds: Record<string, ServiceTimeout> = {
   'concise': { 'default': 15 * 60 },
   'net2cog': {
     'collections': {
-      'C1273831195-ASF': 60 * 60, //  NISAR_L2_GCOV_BETA_V1 — UAT
-      'C3622214170-ASF': 60 * 60, //  NISAR_L2_GCOV_BETA_V1 — PROD
+      'C3622214170-ASF': 60 * 60, // NISAR_L2_GCOV_BETA_V1 - PROD
+      'C2854338529-ASF': 60 * 60, // NISAR_L2_GCOV_PROVISIONAL_V1 - PROD
+      'C3622241360-ASF': 60 * 60, // NISAR_L2_GCOV_V1 - PROD
+      'C1273831195-ASF': 60 * 60, // NISAR_L2_GCOV_BETA_V1 — UAT
+      'C1273428591-ASF': 60 * 60, // NISAR_L2_GCOV_PROVISIONAL_V1 - UAT
+      'C1273428593-ASF': 60 * 60, // NISAR_L2_GCOV_V1 - UAT
     },
   },
 };
