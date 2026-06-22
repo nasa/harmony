c559aa0febdf5ea52f8dd44483d830639a73b5e3
Yonggang Liu <ygliuvt@gmail.com>
2026-06-22 08:54:52 -0400

    HARMONY-2359: Revert the change to fair queueing test.

--- services/harmony/test/work-items/fair-queueing.ts
+++ services/harmony/test/work-items/fair-queueing.ts
@@ -104,8 +104,7 @@ describe('Fair Queueing', function () {
       });
       it('returns the rest of the work items in fair queueing order', function () {
         const jobIds = results.slice(2, 5).map((result) => result.body.workItem?.jobID);
-        expect(jobIds[0]).to.equal('billOldest');
-        expect(jobIds.slice(1)).to.have.members(['bobOldest', 'billMiddle']);
+        expect(jobIds).to.eql(['billOldest', 'bobOldest', 'billMiddle']);
       });
       it('returns a 404 status when no work is available', function () {
         expect(results[5].status).to.equal(404);
