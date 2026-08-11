d4ae72a1c33426c38f45c04ae309755e4df2b2a1
James Norton <james.norton@nasa.gov>
2026-08-11 17:26:53 -0400

    HARMONY-2378: Re-enbled other cron services and added handling for both ministack and AWS for Iceberg

--- services/cron-service/app/util/db/iceberg-connection.ts
+++ services/cron-service/app/util/db/iceberg-connection.ts
@@ -1,19 +1,15 @@
 import { DuckDBConnection } from '@duckdb/node-api';
+import env from '../../util/env';
 
-/**
- * Set up DuckDb connection configuration
- * @param conn - DuckDB connection
- */
-export async function initDbConnection(conn: DuckDBConnection): Promise<void> {
-  const sql = `
-	INSTALL aws;
+const ministackSql = `
+INSTALL aws;
    INSTALL httpfs;
    INSTALL iceberg;
    LOAD aws;
    LOAD httpfs;
    SET TimeZone = 'UTC';
 
-	CREATE SECRET ministack_s3 (
+CREATE SECRET ministack_s3 (
      TYPE s3,
      KEY_ID 'ministack_fake_key',
      SECRET 'ministack_fake_secret',
@@ -21,11 +17,44 @@ export async function initDbConnection(conn: DuckDBConnection): Promise<void> {
      ENDPOINT 'localhost:4566', -- Adjust to your MiniStack container port if different
      USE_SSL false
    );
-	ATTACH 'arn:aws:s3tables:us-west-2:000000000000:bucket/icebeg-tables'
+ATTACH 'arn:aws:s3tables:us-west-2:000000000000:bucket/icebeg-tables'
    AS catalog (
      TYPE iceberg,
      ENDPOINT 'http://localhost:4566/iceberg',
      AUTHORIZATION_TYPE 'none'
    );`;
-  await conn.run(sql);
+   
+const awsSql = `
+INSTALL aws;
+   INSTALL httpfs;
+   INSTALL iceberg;
+   LOAD aws;
+   LOAD httpfs;
+   SET TimeZone = 'UTC';
+
+CREATE SECRET ministack_s3 (
+     TYPE s3,
+     KEY_ID 'ministack_fake_key',
+     SECRET 'ministack_fake_secret',
+     REGION 'us-east-1',
+     ENDPOINT 'localhost:4566', -- Adjust to your MiniStack container port if different
+     USE_SSL false
+   );
+ATTACH 'arn:aws:s3tables:us-west-2:000000000000:bucket/icebeg-tables'
+   AS catalog (
+     TYPE iceberg,
+     ENDPOINT 'http://localhost:4566/iceberg',
+     AUTHORIZATION_TYPE 'none'
+   );`;
+
+/**
+ * Set up DuckDb connection configuration
+ * @param conn - DuckDB connection
+ */
+export async function initDbConnection(conn: DuckDBConnection): Promise<void> {
+  let sql = awsSql;
+  if (env.useLocalstack) {
+    sql = ministackSql;
+  } 
+  await conn.run(ministackSql);
 }
