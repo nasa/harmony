f5f3190a8470766a3f173a9dcc8e47cb8cd36812
James Norton <james.norton@nasa.gov>
2026-08-11 18:19:50 -0400

    HARMONY-2378: Add environment var for Iceberg table bucket arn

--- services/cron-service/app/util/db/iceberg-connection.ts
+++ services/cron-service/app/util/db/iceberg-connection.ts
@@ -32,20 +32,11 @@ INSTALL aws;
    LOAD httpfs;
    SET TimeZone = 'UTC';
 
-CREATE SECRET ministack_s3 (
-     TYPE s3,
-     KEY_ID 'ministack_fake_key',
-     SECRET 'ministack_fake_secret',
-     REGION 'us-east-1',
-     ENDPOINT 'localhost:4566', -- Adjust to your MiniStack container port if different
-     USE_SSL false
-   );
-ATTACH 'arn:aws:s3tables:us-west-2:000000000000:bucket/icebeg-tables'
-   AS catalog (
-     TYPE iceberg,
-     ENDPOINT 'http://localhost:4566/iceberg',
-     AUTHORIZATION_TYPE 'none'
-   );`;
+CREATE SECRET (TYPE s3, PROVIDER credential_chain);
+ATTACH '${env.s3TableBucketArn}' AS catalog (
+   TYPE iceberg,
+   ENDPOINT_TYPE s3_tables
+);`;
 
 /**
  * Set up DuckDb connection configuration
