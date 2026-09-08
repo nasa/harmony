e90a658d23cf31f0e656ebf2ffea5bb16a60e19f
James Norton <james.norton@nasa.gov>
2026-09-08 17:03:17 -0400

    HARMONY-2415: Fix linter issues

--- services/cron-service/app/util/iceberg-row-transforms.ts
+++ services/cron-service/app/util/iceberg-row-transforms.ts
@@ -2,7 +2,7 @@
 export const tableRowTransforms = {
   'workflow_steps': (row: any): void => {
     // remove the access token from the operation
- 
+
     // eslint-disable-next-line @typescript-eslint/dot-notation
     const operationStr = row['operation'];
     const operation = JSON.parse(operationStr);
